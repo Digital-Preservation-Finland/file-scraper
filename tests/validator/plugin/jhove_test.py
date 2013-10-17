2e2d0f2d8111f561c658e9757938b770df7b7eb8
Mikko Vatanen <mikko.vatanen@csc.fi>
2013-10-17 16:13:27 +0300

    KDKPAS-157 Fixed some paths in tests to make Jenkins builds to work

--- tests/validator/plugin/jhove_test.py
+++ tests/validator/plugin/jhove_test.py
@@ -1,7 +1,7 @@
 # Common boilerplate
 import os
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 import pytest
 import testcommon.settings
 
