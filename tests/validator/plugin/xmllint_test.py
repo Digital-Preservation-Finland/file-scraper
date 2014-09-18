55b5f38612004503b07d105315bc964c85e86e57
Juha Törnroos <juha.tornroos@csc.fi>
2014-09-18 16:32:26 +0300

    KDKPAS-536 Implemented DTD and XSD validation for schemas included in XML file to Xmllint validator. This was previosuly done by xmllib validator which is now replaced by Xmllint as default XML file validator in validators.json. Wrote unit tests for every type of potential XML file validation case

--- tests/validator/plugin/xmllint_test.py
+++ tests/validator/plugin/xmllint_test.py
@@ -15,69 +15,159 @@ import subprocess
 ROOTPATH = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
     '../../../'))
-
 SHAREPATH = os.path.join(ROOTPATH, 'include/share')
-
-CATALOGPATH = os.path.join(SHAREPATH, 'schema/catalog-local.xml')
+OBJECT_CATALOGPATH = os.path.join(
+    SHAREPATH,
+    'xmlobjectcatalog/catalog-local.xml')
 SCHEMAPATH = os.path.join(SHAREPATH, 'schema/mets/mets.xsd')
 
 
-class TestMetsValidation:
+class TestXmllintValidation:
 
     testcases = {
-        "test_mets_validation": [
-            {"testcase":   {
-             "name": "CSC_test001",
-                "metspath": '06_mets_validation/sips/CSC_test001/mets.xml'
+        "test_validation": [
+            {"testcase": {
+             "name": "Test XSD validation with KDK METS schema 1",
+                "filepath": '06_mets_validation/sips/CSC_test001/mets.xml',
+                "catalog": OBJECT_CATALOGPATH,
+                "schema": SCHEMAPATH
+                                    
              },
-             "expected":    {
+             "expected": {
                  "returncode": 0,
                  "stdout_has_errors": False,
                  "stderr_has_errors": False,
              }
              },
-            {"testcase":   {
-             "name": "fd2009-00002919-preservation",
-                "metspath": '06_mets_validation/sips/fd2009-00002919-preservation/mets.xml'
+            {"testcase": {
+             "name": "Test XSD validation with KDK METS schema 2",
+                "filepath":
+             '06_mets_validation/sips/fd2009-00002919-preservation/mets.xml',
+                "catalog": OBJECT_CATALOGPATH,
+                "schema": SCHEMAPATH
              },
-             "expected":    {
+             "expected": {
+                 "returncode": 0,
+                 "stdout_has_errors": True,
+                 "stderr_has_errors": False,
+             }
+             },
+            {"testcase": {
+             "name": "Test XSD validation with schema from object catalog",
+                "filepath":
+             '02_filevalidation_data/xml/catalog_schema_valid.xml',
+                "catalog": OBJECT_CATALOGPATH
+             },
+             "expected": {
                  "returncode": 0,
                  "stdout_has_errors": False,
                  "stderr_has_errors": False,
              }
+             },
+            {"testcase": {
+             "name": "Test XSD validation with non existing schema",
+                "filepath":
+             '02_filevalidation_data/xml/catalog_schema_invalid.xml',
+                "catalog": OBJECT_CATALOGPATH
+             },
+             "expected": {
+                 "returncode": 3,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": True,
              }
-
+             },
+            {"testcase": {
+             "name": "Test XSD validation with valid XML file",
+                "filepath": '02_filevalidation_data/xml/valid_xsd.xml',
+                "catalog": OBJECT_CATALOGPATH
+             },
+             "expected": {
+                 "returncode": 0,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": False,
+             }
+             },
+            {"testcase": {
+             "name": "Test XSD validation with invalid XML file",
+                "filepath": '02_filevalidation_data/xml/invalid_xsd.xml',
+                "catalog": OBJECT_CATALOGPATH
+             },
+             "expected": {
+                 "returncode": 3,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": True,
+             }
+             },
+            {"testcase": {
+             "name": "Test validation with well-formed XML file",
+                "filepath": '02_filevalidation_data/xml/valid_wellformed.xml'
+             },
+             "expected": {
+                 "returncode": 0,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": False,
+             }
+             },
+            {"testcase": {
+             "name": "Test validation with not well-formed XML file",
+                "filepath": '02_filevalidation_data/xml/invalid_wellformed.xml'
+             },
+             "expected": {
+                 "returncode": 1,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": True,
+             }
+             },
+            {"testcase": {
+             "name": "Test DTD validation with valid XML file",
+                "filepath": '02_filevalidation_data/xml/valid_dtd.xml'
+             },
+             "expected": {
+                 "returncode": 0,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": False,
+             }
+             },
+            {"testcase": {
+             "name": "Test DTD validation with invalid XML file",
+                "filepath": '02_filevalidation_data/xml/invalid_dtd.xml'
+             },
+             "expected": {
+                 "returncode": 4,
+                 "stdout_has_errors": False,
+                 "stderr_has_errors": True,
+             }
+             }
         ]
     }
 
-    def output_has_error(self, lines):
 
-        for line in lines:
-            if line.find("failed") >= 0:
-                return True
-            if line.find("error") >= 0:
-                return True
+    def test_validation(self, testcase, expected):
+        file_path = os.path.join(
+            testcommon.settings.TESTDATADIR, testcase["filepath"])
 
-        return False
+        validate = validator.plugin.xmllint.Xmllint(
+            "text/xml",
+            "1.0",
+            file_path)
+            
+        if "catalog" in testcase:
+            validate.set_catalog(testcase["catalog"])
+        if "schema" in testcase:
+            validate.add_schema(testcase["schema"])
 
-    def test_mets_validation(self, testcase, expected):
 
-        
-        print "TESTDATADIR", testcommon.settings.TESTDATADIR
-        mets_path = os.path.join(
-            testcommon.settings.TESTDATADIR, testcase["metspath"])
-
-        validate = validator.plugin.xmllint.Xmllint("text/xml", "1.0", mets_path)
-
-        validate.addCatalog(CATALOGPATH)
-        validate.addSchema(SCHEMAPATH)
-    
         (returncode, messages, errors) = validate.validate()
-        print "mets_path", mets_path
-        print returncode, errors
 
         assert returncode == expected["returncode"]
         assert self.output_has_error(
             messages) == expected["stdout_has_errors"]
         assert self.output_has_error(
             errors) == expected["stderr_has_errors"]
+
+
+    def output_has_error(self, lines):
+        if "failed" in lines or "error" in lines:
+            return True
+
+        return False
