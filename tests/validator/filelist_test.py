cc8cc4e0f63a7b27fd42b23aef7391a4657cbb55
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-11-09 19:15:38 +0200

    KDKPAS-1084 Fixed encoding issue.

--- tests/validator/filelist_test.py
+++ tests/validator/filelist_test.py
@@ -1,9 +1,11 @@
+# encoding=utf8
 # Common boilerplate
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(
                 os.path.dirname(__file__), '..')))
-
+reload(sys)
+sys.setdefaultencoding("utf-8")
 import testcommon.settings
 import json
 
