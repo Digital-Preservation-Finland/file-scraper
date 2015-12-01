43dc37c804d7652fb06b2711bd7692b09b2ab4e7
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2015-12-01 18:00:25 +0200

    KDKPAS-1112 Removed stale class and unintended file.

--- tests/validator/plugin/warctools_test.py
+++ tests/validator/plugin/warctools_test.py
@@ -20,46 +20,44 @@ TESTDATADIR = os.path.abspath(os.path.join(TESTDATADIR_BASE,
                                            '02_filevalidation_data'))
 
 
-class TestJWarcToolsFilevalidator:
+def test_validate_warc():
 
-    def test_validate(self):
+    testcasefile = os.path.join(PROJECTDIR, TESTDATADIR,
+                                'warctools_testcases.json')
+    print "\nLoading test configuration from %s\n" % testcasefile
 
-        testcasefile = os.path.join(PROJECTDIR, TESTDATADIR,
-                                    'warctools_testcases.json')
-        print "\nLoading test configuration from %s\n" % testcasefile
+    json_data = open(testcasefile)
+    testcases = json.load(json_data)
+    json_data.close()
 
-        json_data = open(testcasefile)
-        testcases = json.load(json_data)
-        json_data.close()
+    for testcase in testcases["test_validate"]:
 
-        for testcase in testcases["test_validate"]:
+        print "%s: %s" % (testcase["testcase"], testcase["filename"])
 
-            print "%s: %s" % (testcase["testcase"], testcase["filename"])
+        testcase["filename"] = os.path.join(
+            testcommon.settings.TESTDATADIR, testcase["filename"])
+        val = ipt.validator.plugin.warctools.WarcTools(
+            testcase["mimetype"],
+            testcase["formatVersion"],
+            testcase["filename"])
 
-            testcase["filename"] = os.path.join(
-                testcommon.settings.TESTDATADIR, testcase["filename"])
-            val = ipt.validator.plugin.warctools.WarcTools(
-                testcase["mimetype"],
-                testcase["formatVersion"],
-                testcase["filename"])
+        (status, stdout, stderr) = val.validate()
 
-            (status, stdout, stderr) = val.validate()
+        if testcase["expected_result"]["status"] == 0:
+            assert testcase["expected_result"]["status"] == status
+        else:
+            assert testcase["expected_result"]["status"] != 0
 
-            if testcase["expected_result"]["status"] == 0:
-                assert testcase["expected_result"]["status"] == status
-            else:
-                assert testcase["expected_result"]["status"] != 0
+        for match_string in testcase["expected_result"]["stdout"]:
+            message = "\n".join(
+                ["got:", stdout.decode('utf-8'), "expected:",
+                 match_string])
+            assert re.match('(?s).*' + match_string, stdout), message
 
-            for match_string in testcase["expected_result"]["stdout"]:
-                message = "\n".join(
-                    ["got:", stdout.decode('utf-8'), "expected:",
-                     match_string])
-                assert re.match('(?s).*' + match_string, stdout), message
+        for match_string in testcase["expected_result"]["stderr"]:
+            message = "\n".join(
+                ["got:", stderr.decode('utf-8'), "expected:",
+                 match_string])
+            assert re.match('(?s).*' + match_string, stderr), message
 
-            for match_string in testcase["expected_result"]["stderr"]:
-                message = "\n".join(
-                    ["got:", stderr.decode('utf-8'), "expected:",
-                     match_string])
-                assert re.match('(?s).*' + match_string, stderr), message
-
-        return None
+    return None
