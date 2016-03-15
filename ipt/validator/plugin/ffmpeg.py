dcbc3391db65de76fcfff2b21eb78e5527e296c6
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-03-15 09:01:34 +0200

    KDKPAS-971 Created a stub of tests and validator for mpeg files. Added test data for basic ok and rejected case.

---
+++ ipt/validator/plugin/ffmpeg.py
@@ -0,0 +1,30 @@
+""" This is a module that integrates ffmpeg-tool with information-package-tools
+for file validation purposes. Validation is achieved by doing a conversion.
+If conversion is succesful, file is interpred as a valid file."""
+from ipt.utils import run_command
+from ipt.validator.basevalidator import BaseValidator
+
+class FFMpeg(object):
+    """FFMpeg plugin class."""
+
+    def __init__(self, fileinfo):
+        """init"""
+        self.filename = fileinfo['filename']
+        self.fileversion = fileinfo['format']['version']
+        self.mimetype = fileinfo['format']['mimetype']
+        self.exec_cmd = ['ffmpeg',  '-v', 'error', '-i', self.filename,
+            '-f', 'null', '-']
+        self.stdout = ""
+        self.stderr = ""
+        self.exitcode = 1
+
+    def validate(self):
+        """validate file."""
+        print self.exec_cmd
+        (self.exitcode, self.stdout, self.stderr) = run_command(self.exec_cmd)
+
+        if self.stderr:
+            self.exitcode = 117
+        else:
+            self.exitcode = 0
+        return (self.exitcode, self.stdout, self.stderr)
---
+++ tests/validator/plugin/ffmpeg_test.py
@@ -0,0 +1,32 @@
+"""
+This is a test for ffmpeg.py.
+"""
+import os
+import pytest
+
+from ipt.validator.plugin.ffmpeg import FFMpeg
+
+TEST_DATA_DIR_BASE = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../../data'))
+TEST_DATA_PATH = os.path.abspath(
+    os.path.join(TEST_DATA_DIR_BASE, '02_filevalidation_data', 'mpg'))
+
+@pytest.mark.parametrize(
+    ["filename", "stdout", "stderr", "exitcode"],[
+    ("hubble.mpg", "", "", 0),
+    ("hubble_error.mpg","","Missing picture start code",117)])
+def test_mark_ffmpeg(filename, stdout, stderr, exitcode):
+    """FFMpeg test."""
+    file_path = os.path.join(TEST_DATA_PATH, filename)
+    fileinfo = {
+        "filename": file_path,
+        "format": {
+        "version": "2",
+        "mimetype": "video/mpeg"}
+        }
+    validator = FFMpeg(fileinfo=fileinfo)
+    (result_exitcode, result_stdout, result_stderr) = validator.validate()
+
+    assert stdout in result_stdout
+    assert stderr in result_stderr
+    assert exitcode == result_exitcode, result_exitcode
