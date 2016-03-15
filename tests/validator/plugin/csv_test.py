44d61368d78cdc0ee0dfbc3d8e722d7f2fe753ed
Ville-Pekka Vainio <ville-pekka.vainio@csc.fi>
2016-03-15 17:00:49 +0200

    KDKPAS-1227 Update tests
    Remove the test csv file and generate the test data in Python. Add new test
    data based on Wikipedia's CSV article.

--- tests/validator/plugin/csv_test.py
+++ tests/validator/plugin/csv_test.py
@@ -3,6 +3,7 @@ Test for WarcTools.
 """
 import os
 import sys
+import io
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
 import pytest
@@ -24,20 +25,42 @@ JPEG_PATH = os.path.abspath(
         '../../data/06_mets_validation/sips/fd2009-00002919-preservation/access_img/img0008-access.jpg'))
 
 
-def test_validate_success():
-    """Test case for valid csv"""
-    file_path = os.path.join(TESTDATADIR, "mock_data.csv")
-    validator = Csv("text/csv", "UTF-8", file_path)
+def test_validate_success_wikipedia(testpath):
+    """Test the validator with valid data from Wikipedia's CSV article"""
+    csv_path = os.path.join(testpath, 'csv.csv')
+    with io.open(csv_path, 'w', newline='\r\n') as target:
+        target.write(u'1997,Ford,E350,"ac, abs, moon",3000.00\n')
+        target.write(u'1999,Chevy,"Venture ""Extended Edition""","",4900.00\n')
+        target.write(u'1999,Chevy,"Venture ""Extended Edition, Very Large""",,'
+                     '5000.00\n')
+        target.write(u'''1996,Jeep,Grand Cherokee,"MUST SELL!
+air, moon roof, loaded",4799.00''' + u'\n')
+
+    validator = Csv("text/csv", "UTF-8", csv_path)
     (exitcode_result, stdout_result, stderr_result) = validator.validate()
     assert exitcode_result == 0
     assert len(stdout_result) == 0
     assert len(stderr_result) == 0
 
 
-def test_validate_fail():
+def test_validate_fail_jpeg():
     """Test case for invalid csv"""
     # It's hard to get the csv module to fail, feed it a JPG file and it will
     validator = Csv("text/csv", "UTF-8", JPEG_PATH)
+    (exitcode_result, stdout_result, stderr_result) = validator.validate()
+    assert exitcode_result == 1
+    assert len(stdout_result) == 0
+    assert len(stderr_result) != 0
+    print stderr_result
+
+
+def test_validate_fail_wikipedia(testpath):
+    """Test the validator with invalid data based on Wikipedia's CSV article"""
+    csv_path = os.path.join(testpath, 'csv.csv')
+    with io.open(csv_path, 'w', newline='\r\n') as target:
+        target.write(u'1999,Chevy,"Venture ""Extended Edition"","",4900.00\n')
+
+    validator = Csv("text/csv", "UTF-8", csv_path)
     (exitcode_result, stdout_result, stderr_result) = validator.validate()
     assert exitcode_result == 1
     assert len(stdout_result) == 0
