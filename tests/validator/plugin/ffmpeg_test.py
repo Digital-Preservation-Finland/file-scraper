c96b8f18a22284704a7f6249571cf33677dbc0a1
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-03-15 15:06:15 +0200

    KDKPAS-971 PEP8/Pylint fixes.

--- tests/validator/plugin/ffmpeg_test.py
+++ tests/validator/plugin/ffmpeg_test.py
@@ -6,23 +6,24 @@ import pytest
 
 from ipt.validator.plugin.ffmpeg import FFMpeg
 
-TEST_DATA_DIR_BASE = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                                                '../../data'))
+TEST_DATA_DIR_BASE = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../../data'))
 TEST_DATA_PATH = os.path.abspath(
     os.path.join(TEST_DATA_DIR_BASE, '02_filevalidation_data', 'mpg'))
 
+
 @pytest.mark.parametrize(
-    ["filename", "stdout", "stderr", "exitcode"],[
-    ("hubble.mpg", "", "", 0),
-    ("hubble_error.mpg","","Missing picture start code",117)])
+    ["filename", "stdout", "stderr", "exitcode"], [
+        ("hubble.mpg", "", "", 0),
+        ("hubble_error.mpg", "", "Missing picture start code", 117)])
 def test_mark_ffmpeg(filename, stdout, stderr, exitcode):
     """FFMpeg test."""
     file_path = os.path.join(TEST_DATA_PATH, filename)
     fileinfo = {
         "filename": file_path,
         "format": {
-        "version": "2",
-        "mimetype": "video/mpeg"}
+            "version": "2",
+            "mimetype": "video/mpeg"}
         }
     validator = FFMpeg(fileinfo=fileinfo)
     (result_exitcode, result_stdout, result_stderr) = validator.validate()
