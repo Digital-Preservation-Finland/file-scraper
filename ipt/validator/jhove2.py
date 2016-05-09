b8ca07966a88e57835b2be3b3b8d75be5eeadaa5
Juha Törnroos <juha.tornroos@csc.fi>
2016-05-09 15:20:09 +0300

    KDKPAS-1253 Move Jhove2Text validator to jhove2.py file

--- ipt/validator/jhove2.py
+++ ipt/validator/jhove2.py
@@ -1,5 +1,4 @@
-"""Module for vallidating files with JHove 2 Validator"""
-import os
+import subprocess
 import tempfile
 import lxml.etree
 
@@ -7,7 +6,8 @@ from ipt.validator.basevalidator import BaseValidator
 
 NAMESPACES = {'j2': 'http://jhove2.org/xsd/1.0.0'}
 
-class Jhove2(BaseValidator):
+
+class Jhove2Text(BaseValidator):
 
     """ Initializes JHove 2 validator and set ups everything so that
         methods from base class (BaseValidator) can be called, such as
@@ -22,62 +22,60 @@ class Jhove2(BaseValidator):
     """
 
     _supported_mimetypes = {
-        'application/x-internet-archive': [],
-        'application/warc': []
+        text/plain': ['UTF-8']
     }
 
     def __init__(self, fileinfo):
-        super(Jhove2, self).__init__(fileinfo)
+        super(Jhove2Text, self).__init__(fileinfo)
+        self.charset = fileinfo['format']['charset']
+
+    def validate(self):
 
         # Create temp dir for jhove
         tempdir = tempfile.mkdtemp()
-        self.exec_cmd = ['jhove2',  '--display', 'XML', '--temp', tempdir]
-        self.environment['JAVA_OPTS'] = "-Djava.io.tmpdir=%s" % tempdir
 
-        if self.mimetype == 'text/plain':
-            self.charset = fileinfo['format']['charset']
+        # Execute Jhove2
+        cmd = ['jhove2',  '--display', 'XML', '--temp', tempdir]
+        environment = {
+            'JAVA_OPTS': "-Djava.io.tmpdir=%s" % tempdir
+        }
 
-    def check_validity(self):
-        """ Check if file is valid according to JHove output.
+        proc = subprocess.Popen(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=False,
+            env=environment)
 
-            Returns:
-                None if file is valid, otherwise returns JHove's error message.
-        """
+        (stdout, stderr) = proc.communicate()
+        returncode = proc.returncode
 
-        status = self.get_report_field("isValid")
-        filename = self.get_report_field("repInfo")
-        filename = os.path.basename(filename)
+        # Determine validity
+        self.is_valid(returncode)
 
-        if status != 'true':
-            return "ERROR: File '%s' does not validate: %s" % (filename, status)
+        self._check_report()
+        self._check_version()
+        self._check_charset()
 
-        if self.mimetype == 'text/plain':
-            if self.check_charset():
-                return "ERROR: File '%s' does not validate: wrong " \
-                    "charset" % filename
+        return (self.is_valid(),
+                ("\n".join(message) for message in self.messages()),
+                ("\n".join(error) for error in self.errors()))
 
-        return None
+    def _check_report(self):
+        if self.get_report_field("isValid") != 'true':
+            self.not_valid()
+            self.errors.append("ERROR: File '%s' does not validate: %s" %
+                               (self.filename, self.status))
 
-    def check_version(self, version):
-        """ Check if version string matches JHove output.
+    def _check_version(self):
+        found_version = self._get_report_field("FileVersion")
 
-            Arguments:
-                version: version string
+        if found_version != self.version:
+            self.not_valid()
+            self.erros.append("ERROR: File '%s' version is %s, expected %s" %
+                              (self.filename, found_version, self.version))
 
-            Returns:
-                None if version matches, else returns JHove's error message.
-        """
-
-        report_version = self.get_report_field("FileVersion")
-
-        if report_version != version:
-            return "ERROR: File version is '%s', expected '%s'" % (
-                report_version,
-                version)
-        return None
-
-
-    def check_charset(self):
+    def _check_charset(self):
         """Check text files' charset matches with given charset."""
 
         root = lxml.etree.fromstring(self.stdout)
@@ -87,56 +85,18 @@ class Jhove2(BaseValidator):
         results = root.xpath(query, namespaces=NAMESPACES)
 
         if self.charset not in results:
-            return False
+            self.not_valid()
+            self.erros.append("ERROR: File '%s' charset is not expected %s" %
+                              (self.filename, self.charset))
 
-        return True
-
-    def get_report_field(self, field):
-        """ Return field value from JHoves XML output. This method assumes that
-            JHove's XML output handler is used: jhove -h XML. Method uses XPath
-            for querying JHoves output. This method is mainly used by validator
-            class itself. Example usage:
-
-            .. code-block:: python
-
-                get_report_field("Version", report)
-                1.2
-
-                get_report_field("Status", report)
-                "Well formed"
-
-            Arguments:
-                field: Field name which content we are looking for. In practise
-                field is an element in XML document.
-
-            Returns:
-                Concatenated string where each result is on own line. An empty
-                string is returned if there's no results.
+    def _get_report_field(self, field):
+        """
 
         """
         root = lxml.etree.fromstring(self.stdout)
+        query = '//j2:feature[@ftid = "http://jhove2.org/terms/' \
+            'reportable/org/jhove2/module/format/utf8/UTF8Module"]/' \
+            'j2:features/j2:feature[@name="%s"]/j2:value/text()' % field
 
-        query = ""
-        if self.mimetype == 'application/x-internet-archive':
-            query = '//j2:feature[@ftid = "http://jhove2.org/terms/' \
-                    'reportable/org/jhove2/module/format/arc/ArcModule"]/' \
-                    'j2:features/j2:feature[@name="%s"]/j2:value/text()' % field
-        elif self.mimetype == 'text/plain':
-            query = '//j2:feature[@ftid = "http://jhove2.org/terms/' \
-                    'reportable/org/jhove2/module/format/utf8/UTF8Module"]/' \
-                    'j2:features/j2:feature[@name="%s"]/j2:value/text()' % field
-        elif self.mimetype == 'application/warc':
-            query = '//j2:feature[@ftid = "http://jhove2.org/terms/' \
-                    'reportable/org/jhove2/module/format/warc/WarcModule"]/' \
-                    'j2:features/j2:feature[@name="%s"]/j2:value/text()' % field
         results = root.xpath(query, namespaces=NAMESPACES)
-
         return '\n'.join(results)
-
-    def validate(self):
-        """This overrides Basevalidators validate() method because of
-           https://jira.csc.fi/browse/KDKPAS-432 """
-
-        (status, message, error) = super(Jhove2, self).validate()
-
-        return status, "", error
