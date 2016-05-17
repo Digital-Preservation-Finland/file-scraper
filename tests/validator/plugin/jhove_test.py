f9fb481a26cecda9dfae5a8ae9d9fa66e4efd25f
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-05-17 12:45:16 +0300

    KDKPAS-1253 Add charset to tests.

--- tests/validator/plugin/jhove_test.py
+++ tests/validator/plugin/jhove_test.py
@@ -12,53 +12,54 @@ TESTDATADIR = os.path.abspath(
 
 @pytest.mark.usefixtures("monkeypatch_Popen")
 @pytest.mark.parametrize(
-    ["filename", "mimetype", "version", "exitcode", "stdout", "stderr"],
+    ["filename", "mimetype", "version", "charset", "exitcode", "stdout", "stderr"],
     [
         ("test-sips/CSC_test001/kuvat/P1020137.JPG",
-         "image/jpeg", "", True, "Well-Formed and valid", ""),
+         "image/jpeg", None, None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/html/valid.htm",
-         "text/html", "HTML.4.01", True, "Well-Formed and valid", ""),
+         "text/html", "HTML.4.01", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/html/notvalid.htm",
-         "text/html", "HTML.4.01", False, "Well-Formed, but not valid", ""),
+         "text/html", "HTML.4.01", None, False, "Well-Formed, but not valid", ""),
         ("test-sips/CSC_test004/fd2009-00002919-pdf001.pdf",
-         "application/pdf", "1.4", True, "Well-Formed and valid", ""),
+         "application/pdf", "1.4", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/gif_89a/valid.gif",
-         "image/gif", "89a", True, "Well-Formed and valid", ""),
+         "image/gif", "89a", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/gif_89a/invalid.gif",
-         "image/gif", "89a", False, "Not well-formed", ""),
+         "image/gif", "89a", None, False, "Not well-formed", ""),
         ("02_filevalidation_data/gif_87a/valid.gif",
-         "image/gif", "87a", True, "Well-Formed", ""),
+         "image/gif", "87a", None, True, "Well-Formed", ""),
         ("02_filevalidation_data/gif_87a/invalid.gif",
-         "image/gif", "87a", False, "Not well-formed", ""),
+         "image/gif", "87a", None, False, "Not well-formed", ""),
         ("02_filevalidation_data/pdfa-1/valid.pdf",
-         "application/pdf", "1.4", True, "Well-Formed and valid", ""),
+         "application/pdf", "1.4", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/pdfa-1/invalid.pdf",
-         "application/pdf", "1.4", False, "Not well-formed", ""),
+         "application/pdf", "1.4", None, False, "Not well-formed", ""),
         ("02_filevalidation_data/tiff/valid.tif",
-         "image/tiff", "6.0", True, "Well-Formed and valid", ""),
+         "image/tiff", "6.0", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/tiff/invalid.tif",
-         "image/tiff", "6.0", False, "Not well-formed", ""),
+         "image/tiff", "6.0", None, False, "Not well-formed", ""),
         ("02_filevalidation_data/pdf_1_5/sample_1_5.pdf",
-         "application/pdf", "1.5", True, "Well-Formed and valid", ""),
+         "application/pdf", "1.5", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/pdf_1_6/sample_1_6.pdf",
-         "application/pdf", "1.6", True, "Well-Formed and valid", ""),
+         "application/pdf", "1.6", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/tiff/valid_version5.tif",
-         "image/tiff", "6.0", 0, "Well-Formed and valid", ""),
+         "image/tiff", "6.0", None, True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/text/utf8.txt",
-         "text/plain", "UTF-8", 0, "Well-Formed and valid", ""),
+         "text/plain", None, "UTF-8", True, "Well-Formed and valid", ""),
         ("02_filevalidation_data/text/utf8.csv",
-         "text/plain", "UTF-8", 0, "Well-Formed and valid", ""),
+         "text/plain", None, "UTF-8", True, "Well-Formed and valid", ""),
     ])
-def test_validate(filename, mimetype, version, exitcode, stdout, stderr, capsys):
+def test_validate(filename, mimetype, version, charset, exitcode, stdout, stderr, capsys):
     """Test cases of Jhove validation"""
     file_path = os.path.join(TESTDATADIR_BASE, filename)
     fileinfo = {
         "filename": file_path,
         "format": {
             "mimetype": mimetype,
-            "version": version
         }
     }
+    if charset:
+        fileinfo["format"]["charset"] = charset
     if mimetype == 'image/tiff':
         validator = JHoveTiff(fileinfo)
     elif mimetype == 'application/pdf':
