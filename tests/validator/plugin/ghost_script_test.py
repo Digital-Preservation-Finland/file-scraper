59bb95dde6ed9b7ca162d24dcc8d0b3a1f242ba5
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-05-23 12:34:28 +0300

    KDKPAS-1286 Add system error testing.

--- tests/validator/plugin/ghost_script_test.py
+++ tests/validator/plugin/ghost_script_test.py
@@ -4,11 +4,14 @@ Test for pdf 1.7 ghostscript validator.
 
 import os
 import sys
+import pytest
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
 from testcommon.settings import TESTDATADIR
 
 from ipt.validator.ghost_script import GhostScript
+from ipt.utils import UnknownException
 
 BASEPATH = os.path.join(TESTDATADIR, '02_filevalidation_data', 'pdf_1_7')
 
@@ -59,3 +62,22 @@ def test_pdf_1_7_version_error():
     assert 'ERROR: wrong PDF version' in errors
     assert 'PDF document, version 1.3' in messages
     assert validity is False
+
+
+def test_system_error(monkeypatch):
+    """
+    system error test
+    """
+    FILEINFO["filename"] = os.path.join(BASEPATH, "valid_1_7.pdf")
+    validator = GhostScript(FILEINFO)
+    monkeypatch.setattr(validator, 'cmd_exec', ['foo'])
+    with pytest.raises(OSError):
+        validator.validate()
+
+    monkeypatch.setattr(validator, 'file_cmd_exec', ['foo'])
+    with pytest.raises(OSError):
+        validator.validate()
+
+    FILEINFO["filename"] = os.path.join(BASEPATH, "foo.pdf")
+    with pytest.raises(OSError):
+        validator.validate()
