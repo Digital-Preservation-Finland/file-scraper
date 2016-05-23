d8d49acda328260ed001115fa7ede02009c16257
Juha Törnroos <juha.tornroos@csc.fi>
2016-05-23 13:22:12 +0300

    KDKPAS-1311 Implement DummyTextValidator which always returns validation success if mimetype is text/plain and charset ISO-8859-15

---
+++ ipt/validator/dummytextvalidator.py
@@ -0,0 +1,19 @@
+from ipt.validator import BaseValidator
+
+
+class DummyTextValidator(BaseValidator):
+
+    @classmethod
+    def is_supported_mimetype(cls, fileinfo):
+        """
+        Check suported mimetypes.
+        :fileinfo: fileinfo
+        """
+        if fileinfo['format']['mimetype'] == 'text/plain':
+            if fileinfo['format']['charset'] == 'ISO-8859-15':
+                return True
+        return False
+
+    def validate(self):
+        self.messages("")
+        return self.result()
