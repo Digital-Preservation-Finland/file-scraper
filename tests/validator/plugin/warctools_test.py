14a4cfe9d95e31eab1a3f45ed8117483805828a5
tenhunen <aarno.tenhunen@csc.fi>
2016-05-31 16:03:54 +0300

    KDKPAS-1253 Refactor tests according to new validator architecture.

--- tests/validator/plugin/warctools_test.py
+++ tests/validator/plugin/warctools_test.py
@@ -24,29 +24,18 @@ TESTDATADIR = os.path.abspath(os.path.join(TESTDATADIR_BASE,
 
 @pytest.mark.usefixtures("monkeypatch_Popen")
 @pytest.mark.parametrize(
-    ["filename", "mimetype", "version", "validity", "messages", "errors"],
-    [("warc_0_18/warc.0.18.warc", "application/warc", "0.18", True, "", ""),
-     ("warc_0_17/valid.warc", "application/warc", "0.17", True, "",
-        ""),
-     ("warc_0_17/valid.warc", "application/warc", "0.99", False,
-        "File version check error", ""),
-     ("warc_0_17/invalid.warc", "application/warc", "0.17", False,
-        "zero length field name in format", ""),
-     ("warc_1_0/valid.warc.gz", "application/warc", "1.0", True, "", ""),
-     ("warc_1_0/invalid.warc.gz", "application/warc", "1.0", False,
-        "invalid distance code", ""),
+    ["filename", "mimetype", "version", "validity", "messages"],
+    [("warc_0_18/warc.0.18.warc", "application/warc", "0.18", True, ""),
+     ("warc_0_17/valid.warc", "application/warc", "0.17", True, ""),
+     ("warc_1_0/valid.warc.gz", "application/warc", "1.0", True, ""),
      ("arc/valid_arc.gz", "application/x-internet-archive",
-        "1.0", True, "", ""),
+        "1.0", True, ""),
      ("arc/valid_arc_no_compress", "application/x-internet-archive",
-        "1.0", True, "", ""),
-     ("arc/invalid_arc.gz", "application/x-internet-archive",
-        "1.0", False, "Not a gzipped file", ""),
-     ("warc_1_0/valid_no_compress.warc", "application/warc", "1.0", True,
-        "", ""),
-     ("arc/invalid_arc_crc.gz", "application/x-internet-archive", "1.0",
-        False, "CRC check failed", "")])
-def test_validate(filename, mimetype, version, validity, messages, errors):
-    """Test cases for valid/invalid warcs and arcs."""
+        "1.0", True, ""),
+     ("warc_1_0/valid_no_compress.warc", "application/warc", "1.0", True, "")
+     ])
+def test_validate_valid(filename, mimetype, version, validity, messages):
+    """Test cases for valid warcs and arcs."""
 
     fileinfo = {
         "filename": os.path.join(TESTDATADIR, filename),
@@ -57,16 +46,43 @@ def test_validate(filename, mimetype, version, validity, messages, errors):
     }
 
     validator = WarcTools(fileinfo)
-    (validity_result, errors_result, messages_result) = validator.validate()
-    assert validity == validity_result
-    if messages == "":
-        assert messages == messages_result
-    else:
-        assert messages in messages_result
-    if errors == "":
-        assert errors == errors_result
-    else:
-        assert errors in errors_result
+    validator.validate()
+    assert validator.is_valid, validator.errors() + validator.messages()
+    assert 'OK' in validator.messages()
+    assert validator.errors() == ""
+
+
+@pytest.mark.usefixtures("monkeypatch_Popen")
+@pytest.mark.parametrize(
+    ["filename", "mimetype", "version", "validity", "errors"],
+    [("warc_0_17/invalid.warc", "application/warc", "0.17", False,
+        "zero length field name in format"),
+    ("warc_0_17/valid.warc", "application/warc", "0.99", False,
+        "File version check error"),
+    ("warc_1_0/invalid.warc.gz", "application/warc", "1.0", False,
+        "invalid distance code"),
+    ("arc/invalid_arc.gz", "application/x-internet-archive",
+        "1.0", False, "Not a gzipped file"),
+     ("arc/invalid_arc_crc.gz", "application/x-internet-archive", "1.0",
+        False, "CRC check failed")
+     ])
+def test_validate_invalid(filename, mimetype, version, validity, errors):
+    """Test cases for invalid warcs and arcs."""
+
+    fileinfo = {
+        "filename": os.path.join(TESTDATADIR, filename),
+        "format": {
+            "mimetype": mimetype,
+            "version": version
+        }
+    }
+
+    validator = WarcTools(fileinfo)
+    validator.validate()
+    assert not validator.is_valid, validator.errors()
+    assert 'OK' not in validator.messages()
+    assert errors in validator.errors()
+    
 
 
 def test_system_error():
@@ -84,3 +100,4 @@ def test_system_error():
     with pytest.raises(ValidatorError) as error:
         validator.validate()
         assert "No such file or directory" in str(error.value)
+
