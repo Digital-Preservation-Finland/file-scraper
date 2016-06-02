a3f57dd9f54e4438208f59bfa8d7b1a5b6d2fd9e
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-06-02 16:57:03 +0300

    KDKPAS-1253 Refactor JHove validator, remove JHove class and simplify code. Redundant system error checking is removed and its testing. Separate valid and invalid tests.

--- ipt/validator/jhove.py
+++ ipt/validator/jhove.py
@@ -2,7 +2,7 @@
 import os
 import lxml.etree
 
-from ipt.validator.basevalidator import BaseValidator
+from ipt.validator.basevalidator import BaseValidator, Shell
 from ipt.utils import UnknownException, run_command
 
 JHOVE_MODULES = {
@@ -21,148 +21,7 @@ EXTRA_JARS = os.path.join(JHOVE_HOME, '/bin/JhoveView.jar')
 CP = os.path.join(JHOVE_HOME, 'bin/JhoveApp.jar') + ':' + EXTRA_JARS
 
 
-class JHove(BaseValidator):
-    """
-    JHove base class, implement basic functionalities of JHove validation.
-    """
-    _supported_mimetypes = {
-        'image/jp2': [""],
-        'image/gif': ['1987a', '1989a'],
-        'text/html': ['HTML.4.01']
-    }
-
-    def __init__(self, fileinfo):
-        """init.
-        :fileinfo: a dictionary with format
-
-            fileinfo["filename"]
-            fileinfo["algorithm"]
-            fileinfo["digest"]
-            fileinfo["format"]["version"]
-            fileinfo["format"]["mimetype"]
-            fileinfo["format"]["charset"]
-            fileinfo["format"]["format_registry_key"]
-            fileinfo["object_id"]["type"]
-            fileinfo["object_id"]["value"]
-        """
-
-        super(JHove, self).__init__(fileinfo)
-        self.filename = fileinfo['filename']
-        self.mimetype = fileinfo['format']['mimetype']
-        if "version" in fileinfo["format"]:
-            self.fileversion = fileinfo['format']['version']
-
-        validator_module = JHOVE_MODULES[self.mimetype]
-        self.exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
-            validator_module, self.filename]
-        self.statuscode = None
-        self.stdout = None
-        self.stderr = None
-
-    def _check_validity(self):
-        """
-        Check if file is valid according to JHove output.
-        """
-
-        if self.statuscode != 0:
-            self.is_valid(False)
-            self._errors.append(
-                "Validator returned error: %s\n%s" % (
-                    self.statuscode, self.stderr))
-
-        if self.statuscode == 254 or self.statuscode == 255:
-            raise UnknownException("Jhove returned returncode: \
-                %s %s %s" % (self.statuscode, self.stdout, self.stderr))
-
-        status = self.get_report_field("status")
-        filename = os.path.basename(self.filename)
-
-        if status != 'Well-Formed and valid':
-            self._errors.append("ERROR: File '%s' does not validate: %s" % (
-                filename, status))
-            self._errors.append("Validator returned error: %s\n%s" % (
-                self.stdout, self.stderr))
-            self.is_valid(False)
-
-        self._messages.append(status)
-
-    def _check_profile(self):
-        """
-        """
-        pass
-
-    def _check_charset(self):
-        """
-        """
-        pass
-
-    def _check_version(self):
-        """
-        _check_version abstract method
-        Check if version string matches JHove output.
-        """
-        if self.mimetype == 'text/plain':
-            report_version = self.get_report_field("format")
-        else:
-            report_version = self.get_report_field("version")
-            report_version = report_version.replace(" ", ".")
-
-        if report_version != self.fileversion:
-            self.is_valid(False)
-            self._errors.append("ERROR: File version is '%s', expected '%s'"
-                % (report_version, self.fileversion))
-
-    def validate(self):
-        """Validate file with command given in variable self.exec_cmd and with
-        options set in self.exec_options. Also check that validated file
-        version and profile matches with validator.
-
-        :returns: Tuple (validity, messages, errors) where
-            validity -- True is success, False failure, anything else failure
-            messages -- generated report
-            errors -- errors if encountered, else None
-        """
-        (self.statuscode,
-         self.stdout,
-         self.stderr) = run_command(cmd=self.exec_cmd)
-
-        self._check_charset()
-        self._check_validity()
-        self._check_version()
-        messages = "\n".join(message for message in self.messages())
-        errors = "\n".join(error for error in self.errors())
-        return (self.is_valid(), messages, errors)
-
-    def get_report_field(self, field):
-        """
-        Return field value from JHoves XML output. This method assumes that
-        JHove's XML output handler is used: jhove -h XML. Method uses XPath
-        for querying JHoves output. This method is mainly used by validator
-        class itself. Example usage:
-
-        .. code-block:: python
-
-            get_report_field("Version", report)
-            1.2
-
-            get_report_field("Status", report)
-            "Well formed"
-
-        :field: Field name which content we are looking for. In practise
-            field is an element in XML document.
-        :returns:
-            Concatenated string where each result is on own line. An empty
-            string is returned if there's no results.
-        """
-
-        root = lxml.etree.fromstring(self.stdout)
-        query = '//j:%s/text()' % field
-        results = root.xpath(query, namespaces=NAMESPACES)
-
-        return '\n'.join(results)
-
-
-class JHoveTextUTF8(JHove):
+class JHoveTextUTF8(BaseValidator):
     """
     JHove validator fir text/plain UTF-8
     """
@@ -170,13 +29,15 @@ class JHoveTextUTF8(JHove):
         'text/plain': []
     }
 
-    def __init__(self, fileinfo):
+    def validate(self):
         """
-        init.
-        :fileinfo: a dictionary with fileinfo
+        Check if file is valid according to JHove output.
         """
-        super(JHoveTextUTF8, self).__init__(fileinfo)
-        self.charset = fileinfo['format']['charset']
+        validator_module = JHOVE_MODULES[self.fileinfo["format"]["mimetype"]]
+        exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
+            validator_module, self.fileinfo["filename"]]
+        shell = Shell(exec_cmd)
+        check_jhove_errors(shell, self)
 
     @classmethod
     def is_supported_mimetype(cls, fileinfo):
@@ -189,11 +50,8 @@ class JHoveTextUTF8(JHove):
                 return True
         return False
 
-    def _check_version(self):
-        pass
 
-
-class JHovePDF(JHove):
+class JHovePDF(BaseValidator):
     """
     JHove validator for PDF
     """
@@ -202,44 +60,46 @@ class JHovePDF(JHove):
         'application/pdf': ['1.3', '1.4', '1.5', '1.6', 'A-1a', 'A-1b']
     }
 
-    def __init__(self, fileinfo):
+    def validate(self):
         """
-        init.
-        :fileinfo: a dictionary with fileinfo
+        Check if file is valid according to JHove output.
         """
-        super(JHovePDF, self).__init__(fileinfo)
-        self.fileversion = fileinfo['format']['version']
+        validator_module = JHOVE_MODULES[self.fileinfo["format"]["mimetype"]]
+        exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
+            validator_module, self.fileinfo["filename"]]
+        shell = Shell(exec_cmd)
+        check_jhove_errors(shell, self)
+        self._check_version(shell)
 
-    def _check_version(self):
+
+    def _check_version(self, shell):
         """ Check if version string matches JHove output.
         :version: version string
         """
 
-        report_version = self.get_report_field("version")
+        report_version = get_report_field("version", shell)
         report_version = report_version.replace(" ", ".")
-        if "A-1" in self.fileversion:
-            self.fileversion = "1.4"
+        if "A-1" in self.fileinfo["format"]["version"]:
+            self.fileinfo["format"]["version"] = "1.4"
 
-        if report_version != self.fileversion:
-            self.is_valid(False)
-            self._errors.append("ERROR: File version is '%s', expected '%s'"
-                % (report_version, self.fileversion))
+        if report_version != self.fileinfo["format"]["version"]:
+            self.errors("ERROR: File version is '%s', expected '%s'"
+                % (report_version, self.fileinfo["format"]["version"]))
 
     def _check_profile(self):
         """ Check if profile string matches JHove output.
         """
-        if "A-1" not in self.fileversion:
+        if "A-1" not in self.fileinfo["format"]["version"]:
             return
         profile = "ISO PDF/A-1"
-        report_profile = self.get_report_field("profile")
+        report_profile = get_report_field("profile")
         if profile not in report_profile:
-            self.is_valid(False)
-            self._errors.append(
+            self.errors(
                 "ERROR: File profile is '%s', expected '%s'" % (
                     report_profile, profile))
 
 
-class JHoveTiff(JHove):
+class JHoveTiff(BaseValidator):
     """
     JHove validator for tiff
     """
@@ -247,33 +107,35 @@ class JHoveTiff(JHove):
         'image/tiff': ['6.0']
     }
 
-    def __init__(self, fileinfo):
+    def validate(self):
         """
-        init.
-        :fileinfo: a dictionary with fileinfo
+        Check if file is valid according to JHove output.
         """
-        super(JHoveTiff, self).__init__(fileinfo)
-        self.fileversion = fileinfo['format']['version']
+        validator_module = JHOVE_MODULES[self.fileinfo["format"]["mimetype"]]
+        exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
+            validator_module, self.fileinfo["filename"]]
+        shell = Shell(exec_cmd)
+        check_jhove_errors(shell, self)
+        self._check_version(shell)
 
-    def _check_version(self):
-        """ Check if version string matches JHove output.
-        :version: version string
-        :returns: a tuple (0/117, errormessage)
+    def _check_version(self, shell):
+        """
+        Check if version string matches JHove output.
         """
 
-        report_version = self.get_report_field("version")
+        report_version = get_report_field("version", shell)
         report_version = report_version.replace(" ", ".")
         # There is no version tag in TIFF images.
         # TIFF 4.0 and 5.0 is also valid TIFF 6.0.
-        if self.mimetype == "image/tiff" and report_version in ["4.0", "5.0"]:
+        if self.fileinfo["format"]["mimetype"] == "image/tiff" and \
+            report_version in ["4.0", "5.0"]:
             return
-        if report_version != self.fileversion:
-            self.is_valid(False)
-            self._errors.append("ERROR: File version is '%s', expected '%s'"
-                % (report_version, self.fileversion))
+        if report_version != self.fileinfo["format"]["version"]:
+            self.errors("File version is '%s', expected '%s'"
+                % (report_version, self.fileinfo["format"]["version"]))
 
 
-class JHoveJPEG(JHove):
+class JHoveJPEG(BaseValidator):
     """
     JHove validator for JPEG
     """
@@ -281,8 +143,99 @@ class JHoveJPEG(JHove):
         'image/jpeg': [''],
     }
 
-    def _check_version(self):
+    def validate(self):
         """
-        JPEG version is not checked.
+        Check if file is valid according to JHove output.
         """
-        pass
+        validator_module = JHOVE_MODULES[self.fileinfo["format"]["mimetype"]]
+        exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
+            validator_module, self.fileinfo["filename"]]
+        shell = Shell(exec_cmd)
+        check_jhove_errors(shell, self)
+
+
+class JHoveBasic(BaseValidator):
+    """
+    JHove basic class, implement JHove validation.
+    """
+    _supported_mimetypes = {
+        'image/jp2': [""],
+        'image/gif': ['1987a', '1989a'],
+        'text/html': ['HTML.4.01']
+    }
+
+    def validate(self):
+        """
+        Check if file is valid according to JHove output.
+        """
+        validator_module = JHOVE_MODULES[self.fileinfo["format"]["mimetype"]]
+        exec_cmd = ['java', '-classpath', CP, 'Jhove', '-h', 'XML', '-m',
+            validator_module, self.fileinfo["filename"]]
+        shell = Shell(exec_cmd)
+        check_jhove_errors(shell, self)
+        self._check_version(shell)
+        
+
+    def _check_version(self, shell):
+        """
+        _check_version abstract method
+        Check if version string matches JHove output.
+        """
+        if self.fileinfo["format"]["mimetype"] == 'text/plain':
+            report_version = get_report_field("format", shell)
+        else:
+            report_version = get_report_field("version", shell)
+            report_version = report_version.replace(" ", ".")
+
+        if report_version != self.fileinfo["format"]["version"]:
+            self.errors("ERROR: File version is '%s', expected '%s'"
+                % (report_version, self.fileinfo["format"]["version"]))
+
+def check_jhove_errors(shell, validator):
+    """
+    """
+    if shell.returncode != 0:
+        validator.errors("Validator returned error: %s\n%s" % (
+            shell.returncode, shell.stderr))
+        print "RET", shell.returncode
+
+    status = get_report_field("status", shell)
+    filename = os.path.basename(validator.fileinfo["filename"])
+
+    if status != 'Well-Formed and valid':
+        validator.errors("File '%s' does not validate: %s" % (
+            filename, status))
+        validator.errors("Validator returned error: %s\n%s" % (
+            shell.stdout, shell.stderr))
+
+    validator.messages(status)
+
+
+def get_report_field(field, shell):
+    """
+    Return field value from JHoves XML output. This method assumes that
+    JHove's XML output handler is used: jhove -h XML. Method uses XPath
+    for querying JHoves output. This method is mainly used by validator
+    class itself. Example usage:
+
+    .. code-block:: python
+
+        get_report_field("Version", report)
+        1.2
+
+        get_report_field("Status", report)
+        "Well formed"
+
+    :field: Field name which content we are looking for. In practise
+        field is an element in XML document.
+    :returns:
+        Concatenated string where each result is on own line. An empty
+        string is returned if there's no results.
+    """
+    print type(shell.stdout), shell.stderr, shell.stdout
+    root = lxml.etree.fromstring(shell.stdout)
+    query = '//j:%s/text()' % field
+    results = root.xpath(query, namespaces=NAMESPACES)
+
+    return '\n'.join(results)
+
--- tests/validator/plugin/jhove_test.py
+++ tests/validator/plugin/jhove_test.py
@@ -2,7 +2,7 @@
 import os
 import pytest
 
-from ipt.validator.jhove import JHove, JHoveTiff, JHovePDF, JHoveTextUTF8, \
+from ipt.validator.jhove import JHoveBasic, JHoveTiff, JHovePDF, JHoveTextUTF8, \
     JHoveJPEG
 
 TESTDATADIR_BASE = os.path.abspath(
@@ -13,46 +13,36 @@ TESTDATADIR = os.path.abspath(
 
 @pytest.mark.usefixtures("monkeypatch_Popen")
 @pytest.mark.parametrize(
-    ["filename", "mimetype", "version", "charset", "exitcode", "stdout", "stderr"],
+    ["validator_class", "filename", "mimetype", "version", "charset", "exitcode", "stdout", "stderr"],
     [
-        ("test-sips/CSC_test001/kuvat/P1020137.JPG",
+        (JHoveJPEG, "test-sips/CSC_test001/kuvat/P1020137.JPG",
          "image/jpeg", None, None, True, "Well-Formed and valid", ""),
-        ("test-sips/CSC_test001/kuvat/P1020137.JPG",
+        (JHoveJPEG, "test-sips/CSC_test001/kuvat/P1020137.JPG",
          "image/jpeg", "1.01", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/html/valid.htm",
+        (JHoveBasic, "02_filevalidation_data/html/valid.htm",
          "text/html", "HTML.4.01", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/html/notvalid.htm",
-         "text/html", "HTML.4.01", None, False, "Well-Formed, but not valid", ""),
-        ("test-sips/CSC_test004/fd2009-00002919-pdf001.pdf",
+        (JHovePDF, "test-sips/CSC_test004/fd2009-00002919-pdf001.pdf",
          "application/pdf", "1.4", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/gif_89a/valid.gif",
+        (JHoveBasic, "02_filevalidation_data/gif_89a/valid.gif",
          "image/gif", "89a", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/gif_89a/invalid.gif",
-         "image/gif", "89a", None, False, "Not well-formed", ""),
-        ("02_filevalidation_data/gif_87a/valid.gif",
+        (JHoveBasic, "02_filevalidation_data/gif_87a/valid.gif",
          "image/gif", "87a", None, True, "Well-Formed", ""),
-        ("02_filevalidation_data/gif_87a/invalid.gif",
-         "image/gif", "87a", None, False, "Not well-formed", ""),
-        ("02_filevalidation_data/pdfa-1/valid.pdf",
+        (JHovePDF, "02_filevalidation_data/pdfa-1/valid.pdf",
          "application/pdf", "1.4", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/pdfa-1/invalid.pdf",
-         "application/pdf", "1.4", None, False, "Not well-formed", ""),
-        ("02_filevalidation_data/tiff/valid.tif",
+        (JHoveTiff, "02_filevalidation_data/tiff/valid.tif",
          "image/tiff", "6.0", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/tiff/invalid.tif",
-         "image/tiff", "6.0", None, False, "Not well-formed", ""),
-        ("02_filevalidation_data/pdf_1_5/sample_1_5.pdf",
+        (JHovePDF, "02_filevalidation_data/pdf_1_5/sample_1_5.pdf",
          "application/pdf", "1.5", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/pdf_1_6/sample_1_6.pdf",
+        (JHovePDF, "02_filevalidation_data/pdf_1_6/sample_1_6.pdf",
          "application/pdf", "1.6", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/tiff/valid_version5.tif",
+        (JHoveTiff, "02_filevalidation_data/tiff/valid_version5.tif",
          "image/tiff", "6.0", None, True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/text/utf8.txt",
+        (JHoveTextUTF8, "02_filevalidation_data/text/utf8.txt",
          "text/plain", None, "UTF-8", True, "Well-Formed and valid", ""),
-        ("02_filevalidation_data/text/utf8.csv",
-         "text/plain", None, "UTF-8", True, "Well-Formed and valid", ""),
+        (JHoveTextUTF8, "02_filevalidation_data/text/utf8.csv",
+         "text/plain", None, "UTF-8", True, "Well-Formed and valid", "")
     ])
-def test_validate(filename, mimetype, version, charset, exitcode, stdout, stderr, capsys):
+def test_validate_valid(validator_class, filename, mimetype, version, charset, exitcode, stdout, stderr, capsys):
     """Test cases of Jhove validation"""
     file_path = os.path.join(TESTDATADIR_BASE, filename)
     fileinfo = {
@@ -66,40 +56,48 @@ def test_validate(filename, mimetype, version, charset, exitcode, stdout, stderr
     if version:
         fileinfo["format"]["version"] = version
 
-    if mimetype == 'image/tiff':
-        validator = JHoveTiff(fileinfo)
-    elif mimetype == 'application/pdf':
-        validator = JHovePDF(fileinfo)
-    elif mimetype == 'text/plain':
-        validator = JHoveTextUTF8(fileinfo)
-    elif mimetype == 'image/jpeg':
-        validator = JHoveJPEG(fileinfo)
-    else:
-        validator = JHove(fileinfo)
+    validator = validator_class(fileinfo)
     print capsys.readouterr()
-    (exitcode_result, stdout_result, stderr_result) = validator.validate()
-    assert exitcode == exitcode_result
-    assert stdout in stdout_result
-    assert stderr in stderr_result
-
+    validator.validate()
+    assert validator.is_valid
+    assert stdout in validator.messages()
+    assert stderr == ""
 
 @pytest.mark.usefixtures("monkeypatch_Popen")
-def test_system_error(monkeypatch):
-    """
-    Test for system error(missing file)
-    """
+@pytest.mark.parametrize(
+    ["validator_class", "filename", "mimetype", "version", "charset",
+     "exitcode", "stdout", "stderr"],
+    [
+        (JHoveBasic, "02_filevalidation_data/html/notvalid.htm",
+         "text/html", "HTML.4.01", None, False, "Well-Formed, but not valid", ""),
+        (JHoveBasic, "02_filevalidation_data/gif_89a/invalid.gif",
+         "image/gif", "89a", None, False, "Not well-formed", ""),
+        (JHoveBasic, "02_filevalidation_data/gif_87a/invalid.gif",
+         "image/gif", "87a", None, False, "Not well-formed", ""),
+        (JHovePDF, "02_filevalidation_data/pdfa-1/invalid.pdf",
+         "application/pdf", "1.4", None, False, "Not well-formed", ""),
+        (JHoveTiff, "02_filevalidation_data/tiff/invalid.tif",
+         "image/tiff", "6.0", None, False, "Not well-formed", ""),
+    ])
 
-    # jhove command not found
-    with pytest.raises(OSError):
-        file_path = os.path.join(TESTDATADIR, "gif_87a/valid.gif")
-        fileinfo = {
-            "filename": file_path,
-            "format": {
-                "mimetype": "image/gif",
-                "version": "1.0"
-            }
+def test_validate_invalid(validator_class, filename, mimetype, version,
+        charset, exitcode, stdout, stderr, capsys):
+    """Test cases of Jhove validation"""
+    file_path = os.path.join(TESTDATADIR_BASE, filename)
+    fileinfo = {
+        "filename": file_path,
+        "format": {
+            "mimetype": mimetype,
         }
+    }
+    if charset:
+        fileinfo["format"]["charset"] = charset
+    if version:
+        fileinfo["format"]["version"] = version
 
-        validator = JHove(fileinfo)
-        monkeypatch.setattr(validator, 'exec_cmd', ['foo'])
-        validator.validate()
+    validator = validator_class(fileinfo)
+    print capsys.readouterr()
+    validator.validate()
+    assert not validator.is_valid
+    assert stdout in validator.messages()
+    assert stderr in validator.errors()
