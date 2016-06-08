8c68a446c99ff1386e97f4cbfb3ea89dce8b76ab
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-06-08 12:32:56 +0300

    KDKPAS-1253 Remove redundant class.

--- tests/validator/plugin/xmllint_test.py
+++ tests/validator/plugin/xmllint_test.py
@@ -19,76 +19,74 @@ OBJECT_CATALOGPATH = os.path.join(
 SCHEMAPATH = os.path.join(SHAREPATH, 'schema/mets/mets.xsd')
 
 
-class TestXmllintValidation:
+@pytest.mark.usefixtures("monkeypatch_Popen")
+@pytest.mark.parametrize(
+    ["filename", "schema"],
+    [
+        ("06_mets_validation/sips/CSC_test001/mets.xml", True),
+        ("06_mets_validation/sips/fd2009-00002919-preservation/mets.xml",
+         True),
+        ("02_filevalidation_data/xml/catalog_schema_valid.xml", False),
+        ("02_filevalidation_data/xml/valid_xsd.xml", False),
+        ("02_filevalidation_data/xml/valid_wellformed.xml", False),
+        ("02_filevalidation_data/xml/valid_dtd.xml", False),
+    ])
+def test_validation_valid(filename, schema, capsys):
+    """
+    test valid cases
+    """
+    fileinfo = {
+        "filename": os.path.join(
+            testcommon.settings.TESTDATADIR, filename),
+        "format": {
+            "mimetype": "text/xml",
+            "version": "1.0"
+        },
+    }
 
-    @pytest.mark.usefixtures("monkeypatch_Popen")
-    @pytest.mark.parametrize(
-        ["filename", "schema"],
-        [
-            ("06_mets_validation/sips/CSC_test001/mets.xml", True),
-            ("06_mets_validation/sips/fd2009-00002919-preservation/mets.xml", True),
-            ("02_filevalidation_data/xml/catalog_schema_valid.xml", False),
-            ("02_filevalidation_data/xml/valid_xsd.xml", False),
-            ("02_filevalidation_data/xml/valid_wellformed.xml", False),
-            ("02_filevalidation_data/xml/valid_dtd.xml", False),
-        ])
-    def test_validation_valid(self, filename, schema, capsys):
-        """
-        test valid cases
-        """
-        fileinfo = {
-            "filename": os.path.join(
-                testcommon.settings.TESTDATADIR, filename),
-            "format": {
-                "mimetype": "text/xml",
-                "version": "1.0"
-            },
-        }
+    if schema is True:
+        fileinfo["schema"] = SCHEMAPATH
 
-        if schema is True:
-            fileinfo["schema"] = SCHEMAPATH
+    validator = ipt.validator.xmllint.Xmllint(fileinfo)
 
-        validator = ipt.validator.xmllint.Xmllint(fileinfo)
-
-        validator.validate()
-        print capsys.readouterr()
-        assert validator.is_valid
-        assert "Validation success" in validator.messages()
-        assert validator.errors() == ""
-        # xmllint is using --noout, so the METS XML should not be printed to
-        # stdout (KDKPAS-1190)
-        assert "mets:mets" not in validator.messages()
+    validator.validate()
+    print capsys.readouterr()
+    assert validator.is_valid
+    assert "Validation success" in validator.messages()
+    assert validator.errors() == ""
+    # xmllint is using --noout, so the METS XML should not be printed to
+    # stdout (KDKPAS-1190)
+    assert "mets:mets" not in validator.messages()
 
 
-    @pytest.mark.usefixtures("monkeypatch_Popen")
-    @pytest.mark.parametrize(
-        "filename",
-        [
-            ("02_filevalidation_data/xml/catalog_schema_invalid.xml"),
-            ("02_filevalidation_data/xml/invalid_xsd.xml"),
-            ("02_filevalidation_data/xml/invalid_wellformed.xml"),
-            ("02_filevalidation_data/xml/invalid_dtd.xml")
-        ])
-    def test_validation_invalid(self, filename, capsys):
-        """
-        test valid cases
-        """
-        fileinfo = {
-            "filename": os.path.join(
-                testcommon.settings.TESTDATADIR, filename),
-            "format": {
-                "mimetype": "text/xml",
-                "version": "1.0"
-            },
-        }
+@pytest.mark.usefixtures("monkeypatch_Popen")
+@pytest.mark.parametrize(
+    "filename",
+    [
+        ("02_filevalidation_data/xml/catalog_schema_invalid.xml"),
+        ("02_filevalidation_data/xml/invalid_xsd.xml"),
+        ("02_filevalidation_data/xml/invalid_wellformed.xml"),
+        ("02_filevalidation_data/xml/invalid_dtd.xml")
+    ])
+def test_validation_invalid(filename, capsys):
+    """
+    test valid cases
+    """
+    fileinfo = {
+        "filename": os.path.join(
+            testcommon.settings.TESTDATADIR, filename),
+        "format": {
+            "mimetype": "text/xml",
+            "version": "1.0"
+        },
+    }
 
-        validator = ipt.validator.xmllint.Xmllint(fileinfo)
+    validator = ipt.validator.xmllint.Xmllint(fileinfo)
 
-        validator.validate()
-        print capsys.readouterr()
-        assert not validator.is_valid
+    validator.validate()
+    print capsys.readouterr()
+    assert not validator.is_valid
 
-        # xmllint is using --noout, so the METS XML should not be printed to
-        # stdout (KDKPAS-1190)
-        assert "mets:mets" not in validator.messages()
-
+    # xmllint is using --noout, so the METS XML should not be printed to
+    # stdout (KDKPAS-1190)
+    assert "mets:mets" not in validator.messages()
