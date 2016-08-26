275d78c229f6cf65a0952750056050f3d4592891
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-08-26 16:11:56 +0300

    KDKPAS-971 Refactor tests according to new BasValidator. Separate ok an nok cases. Remove redundant test data.

--- tests/validator/plugin/ffmpeg_test.py
+++ tests/validator/plugin/ffmpeg_test.py
@@ -13,20 +13,11 @@ TEST_DATA_PATH = os.path.abspath(
 
 
 @pytest.mark.parametrize(
-    ["mimetype", "filename", "version", "stdout", "stderr", "exitcode"], [
-        ("video/mpeg", "system_error", "1", "", "", 1),
-        ("video/mpeg", "mpg1.mpg", "1", "", "", 0),
-        ("video/mpeg", "mpg1_error.mpg", "1", "", "Missing picture start code", 117),
-        ("video/mpeg", "mpg1_error2.mpg", "1", "", "", 117),
-        ("video/mp4", "mp4.mp4", "", "", "", 0),
-        ("video/mp4", "mp4_error.mp4", "", "", "Invalid data found", 117),
-        ("video/mpeg", "mpg2.mpg", "2", "", "", 0),
-        ("video/mpeg", "mpg2_error.mpg", "2", "", "", 117),
-        ("video/mpeg", "mpg1.mpg", "4", "", "Wrong format version", 117),
-        ("video/mpeg", "unknown_mimetype.3gp", "2", "", "Unknown mimetype or version", 117),
-        ("video/mpeg", "no_video.wav", "1", "", "No version information could be found",
-            117)])
-def test_mark_ffmpeg(mimetype, filename, version, stdout, stderr, exitcode):
+    ["mimetype", "filename", "version"], [
+        ("video/mpeg", "mpg1.mpg", "1"),
+        ("video/mp4", "mp4.mp4", ""),
+        ("video/mpeg", "mpg2.mpg", "2")])
+def test_mark_ffmpeg_ok(mimetype, filename, version):
     """FFMpeg test."""
     file_path = os.path.join(TEST_DATA_PATH, filename)
     fileinfo = {
@@ -36,8 +27,35 @@ def test_mark_ffmpeg(mimetype, filename, version, stdout, stderr, exitcode):
             "mimetype": mimetype}
         }
     validator = FFMpeg(fileinfo=fileinfo)
-    (result_exitcode, result_stdout, result_stderr) = validator.validate()
+    validator.validate()
 
-    assert stdout in result_stdout
-    assert stderr in result_stderr
-    assert exitcode == result_exitcode
+    assert 'Error' not in validator.messages()
+    assert validator.messages() != ""
+    assert validator.is_valid
+    assert validator.errors() == ""
+
+
+@pytest.mark.parametrize(
+    ["mimetype", "filename", "version"], [
+        ("video/mpeg", "mpg1_error.mpg", "1"),
+        ("video/mpeg", "mpg1_error2.mpg", "1"),
+        ("video/mp4", "mp4_error.mp4", ""),
+        ("video/mpeg", "mpg2_error.mpg", "2"),
+        ("video/mpeg", "mpg1.mpg", "4"),
+        ("video/mpeg", "unknown_mimetype.3gp", "2"),
+        ("video/mpeg", "no_video.wav", "1")])
+def test_mark_ffmpeg_nok(mimetype, filename, version):
+    """FFMpeg test."""
+    file_path = os.path.join(TEST_DATA_PATH, filename)
+    fileinfo = {
+        "filename": file_path,
+        "format": {
+            "version": version,
+            "mimetype": mimetype}
+        }
+    validator = FFMpeg(fileinfo=fileinfo)
+    validator.validate()
+
+    assert 'Error' in validator.messages()
+    assert not validator.is_valid
+    assert validator.errors() != ""
