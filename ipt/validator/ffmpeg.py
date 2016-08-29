b027dc69c3470ee7a18599bd0803b8335d3505c1
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-08-29 10:33:48 +0300

    KDKPAS-971 Refactor test according to new BaseValidator. Validation logic has been improved and the inclusive error list is not used anymore. Instead, if there is anything stderr, file is invalid.

--- ipt/validator/ffmpeg.py
+++ ipt/validator/ffmpeg.py
@@ -1,7 +1,7 @@
 """ This is a module that integrates ffmpeg-tool with information-package-tools
 for file validation purposes. Validation is achieved by doing a conversion.
 If conversion is succesful, file is interpred as a valid file."""
-from ipt.validator.basevalidator import BaseValidator
+from ipt.validator.basevalidator import BaseValidator, Shell
 from ipt.utils import run_command
 
 FORMATS = [
@@ -32,55 +32,32 @@ class FFMpeg(BaseValidator):
         'video/mp4': ['']
     }
 
-    def __init__(self, fileinfo):
-        """init"""
-        self.filename = fileinfo['filename']
-        self.file_format = fileinfo['format']
-        self.validation_cmd = [
-            'ffmpeg', '-v', 'error', '-i', self.filename, '-f', 'null', '-']
-        self.version_cmd = ['ffprobe', self.filename]
-        self.stdout = []
-        self.stderr = []
-        self.exitcode = []
-
     def validate(self):
         """validate file."""
         self.check_version()
         self.check_validity()
-        if set(self.exitcode).issubset(set([0])):
-            validity = 0
-        elif not set(self.exitcode).issubset([0, 117]):
-            validity = 1
-        else:
-            validity = 117
-        return (validity, '\n'.join(self.stdout), '\n'.join(self.stderr))
 
     def check_version(self):
         """parse version from stderr, which is in such format:
 
             Stream #0:0[0x1e0]: Video: mpeg2video (Main), yuv420p, ...
         """
-        (_, _, stderr) = run_command(self.version_cmd)
-        self.check_system_errors(stderr)
-        if "Video: " not in stderr:
-            self.append_results(
-                117, "", "No version information could be found,"
-                "file might not be video: %s" % stderr)
+        shell = Shell(['ffprobe', self.fileinfo['filename']])
+        if "Video: " not in shell.stderr:
+            self.errors(
+                "No version information could be found,"
+                "file might not be video: %s" % shell.stderr)
             return
 
-        detected_format = self.resolve_format(stderr)
-        if not detected_format:
-            return
-        if detected_format == self.file_format:
-            self.append_results(0, "", "")
-        else:
-            self.append_results(
-                117, "", "Wrong format version, got %s version %s, "
+        detected_format = self.resolve_format(shell.stderr)
+        if detected_format != self.fileinfo["format"] and detected_format:
+            self.errors(
+                "Wrong format version, got %s version %s, "
                 "expected %s version %s." % (
                     detected_format["mimetype"],
                     detected_format["version"],
-                    self.file_format["mimetype"],
-                    self.file_format["version"]))
+                    self.fileinfo["format"]["mimetype"],
+                    self.fileinfo["format"]["version"]))
 
     def resolve_format(self, info):
         """Resolve format name and version."""
@@ -88,17 +65,16 @@ class FFMpeg(BaseValidator):
         for format_ in FORMATS:
             if format_["format_string"] == line:
                 return format_["format"]
-        self.append_results(117, "", "Unknown mimetype or version")
+        self.errors("Unknown mimetype or version")
 
     def check_validity(self):
         """Check file validity."""
-        (exitcode, stdout, stderr) = run_command(self.validation_cmd)
-        self.check_system_errors(stderr)
-        if stderr:
-            exitcode = 117
-        for error in SYSTEM_ERRORS:
-            if error in stderr:
-                print "SYSTEM_ERRORS"
-                exitcode = 1
-                break
-        self.append_results(exitcode, stdout, stderr)
+        shell = Shell([
+            'ffmpeg', '-v', 'error', '-i', self.fileinfo['filename'],
+            '-f', 'null', '-'])
+        if shell.returncode != 0 or shell.stderr != "":
+            self.errors(
+                "File %s not valid: %s" % (
+                    self.fileinfo['filename'], shell.stderr))
+            return
+        self.messages("OK")
