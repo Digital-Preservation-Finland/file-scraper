260ee04f5643c6d7e44e68b5312f829701c40ced
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-09-09 13:51:13 +0300

    KDKPAS-971 Add check for streams against mets metadata. Streams are now printed in json from ffprobe.

--- ipt/validator/ffmpeg.py
+++ ipt/validator/ffmpeg.py
@@ -4,7 +4,10 @@ achieved by doing a conversion. If conversion is succesful, file is
 interpred as a valid file. Container type is also validated with ffprobe.
 """
 
+import json
+
 from ipt.validator.basevalidator import BaseValidator, Shell
+from ipt.utils import compare_lists_of_dicts
 
 
 class FFMpeg(BaseValidator):
@@ -15,9 +18,27 @@ class FFMpeg(BaseValidator):
         'video/mp4': ['']
     }
 
+    stream_strings = {
+        "mpegvideo": "MPEG 1",
+        "mpeg1video": "MPEG 1",
+        "mpeg2video": "MPEG 2",
+        "h264": "AVC",
+        "libmp3lame": "MP3",
+        "libshine": "MP3",
+        "mp3": "MP3",
+        "mp3adu": "MP3",
+        "mp3adufloat": "MP3",
+        "mp3float": "MP3",
+        "mp3on4": "MP3",
+        "mp3on4float": "MP3",
+        "aac": "AAC"
+    }
+
     def validate(self):
         """validate file."""
         self.check_version()
+        self.check_streams("audiomd")
+        self.check_streams("videomd")
         self.check_validity()
 
     def check_version(self):
@@ -94,3 +115,31 @@ class FFMpeg(BaseValidator):
                     self.fileinfo['filename'], shell.stderr))
             return
         self.messages("%s is valid." % self.fileinfo['filename'])
+
+    def check_streams(self, stream_type):
+        """Check that streams inside container are what they are described in
+        audioMD and videoMD.
+        :stream_type: "audiomd" or "videomd"
+        """
+        if stream_type not in self.fileinfo:
+            return
+        streams = {stream_type: []}
+        shell = Shell(
+            ['ffprobe', '-show_streams', '-show_format', '-print_format',
+             'json', self.fileinfo['filename']])
+
+        stream_raw_data = json.loads(shell.stdout)
+
+        for stream in stream_raw_data["streams"]:
+            if stream["codec_type"] == stream_type:
+                streams[stream_type].append({"codec": stream["codec_name"]})
+        (missing, extra) = compare_lists_of_dicts(
+            self.fileinfo[stream_type], streams[stream_type])
+        if missing or extra:
+            self.errors("Streams in container %s are not what is "
+                        "described in metadata. Found %s, expected %s" % (
+                            self.fileinfo["filename"],
+                            streams[stream_type],
+                            self.fileinfo[stream_type]))
+        self.messages("Streams %s are according to metadata description" %
+                      self.fileinfo[stream_type])
