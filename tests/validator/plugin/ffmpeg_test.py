ed1503ef6da77a6be8d06e6dde3ab5f53757459c
Aarno Tenhunen <aarno.tenhunen@csc.fi>
2016-09-23 13:09:12 +0300

    KDKPAS-971 Add test data according to new fileinfo.

--- tests/validator/plugin/ffmpeg_test.py
+++ tests/validator/plugin/ffmpeg_test.py
@@ -41,20 +41,48 @@ def test_mark_ffmpeg_ok():
         filename="mpg1.mpg",
         mimetype="video/mpeg",
         version="1",
-        videomd=[{"codec": "MPEG 1"}])
+        videomd=[
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': '-99',
+             'duration': '19.025400',
+             'width': '320',
+             'display_aspect_ratio': '4:3',
+             'height': '240',
+             'avg_frame_rate': '30000/1001'}])
 
     check_ffmpeg_ok(
         filename="mpg2.mpg",
         mimetype="video/mpeg",
         version="2",
-        videomd=[{"codec": "MPEG 2"}])
+        videomd=[
+            {"codec_name": "MPEG 2",
+             "width": '320',
+             "height": '240',
+             "sample_aspect_ratio": "1:1",
+             "display_aspect_ratio": "4:3",
+             "level": '8',
+             "avg_frame_rate": "30000/1001",
+             "duration": "18.985633"}])
 
     check_ffmpeg_ok(
         filename="mp4.mp4",
         mimetype="video/mp4",
         version="",
-        videomd=[{"codec": "AVC"}],
-        audiomd=[{"codec": "AAC"}])
+        videomd=[
+            {"codec_name": "AVC",
+             "width": '1280',
+             "height": '720',
+             "sample_aspect_ratio": "1:1",
+             "display_aspect_ratio": "16:9",
+             "level": '31',
+             "avg_frame_rate": "25/1",
+             "duration": "5.280000"}],
+        audiomd=[
+            {"codec_name": "AAC",
+             "sample_rate": "48000",
+             "duration": "5.312000",
+             "channels": "6"}])
 
 
 def check_ffmpeg_nok(filename, mimetype, version, videomd=None, audiomd=None):
@@ -86,53 +114,133 @@ def test_mark_ffmpeg_nok():
     """
     Test for failed validation
     """
+
     check_ffmpeg_nok(
         filename="mpg1_error.mpg",
         mimetype="video/mpeg",
         version="1",
-        videomd=[{"codec": "MPEG 1"}])
+        videomd=[
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': -99,
+             'duration': '19.025400',
+             'width': 320,
+             'codec_long_name': 'MPEG-1 video',
+             'display_aspect_ratio': '4:3',
+             'height': 240,
+             'avg_frame_rate': '30000/1001'}])
 
     check_ffmpeg_nok(
         filename="mpg1_error2.mpg",
         mimetype="video/mpeg",
         version="1",
-        videomd=[{"codec": "MPEG 1"}])
+        videomd=[
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': -99,
+             'duration': '19.025400',
+             'width': 320,
+             'codec_long_name': 'MPEG-1 video',
+             'display_aspect_ratio': '4:3',
+             'height': 240,
+             'avg_frame_rate': '30000/1001'}])
 
     check_ffmpeg_nok(
         filename="mp4_error.mp4",
         mimetype="video/mp4",
-        version="")
+        version="",
+        videomd=[
+            {"codec_name": "AVC",
+             "width": '1280',
+             "height": '720',
+             "sample_aspect_ratio": "1:1",
+             "display_aspect_ratio": "16:9",
+             "level": '31',
+             "avg_frame_rate": "25/1",
+             "duration": "5.280000"}],
+        audiomd=[
+            {"codec_name": "AAC",
+             "sample_rate": "48000",
+             "duration": "5.312000",
+             "channels": "6"}])
 
     check_ffmpeg_nok(
         filename="mpg2_error.mpg",
         mimetype="video/mpeg",
         version="2",
-        videomd=[{"codec": "MPEG 2"}])
+        videomd=[
+            {"codec_name": "MPEG 2",
+             "width": '320',
+             "height": '240',
+             "sample_aspect_ratio": "1:1",
+             "display_aspect_ratio": "4:3",
+             "level": '8',
+             "avg_frame_rate": "30000/1001",
+             "duration": "18.985633"}])
 
     check_ffmpeg_nok(
         filename="mpg1.mpg",
         mimetype="video/mpeg",
         version="4",
-        videomd=[{"codec": "MPEG 1"}])
+        videomd=[
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': '-99',
+             'duration': '19.025400',
+             'width': '320',
+             'display_aspect_ratio': '4:3',
+             'height': '240',
+             'avg_frame_rate': '30000/1001'}])
 
     check_ffmpeg_nok(
         filename="unknown_mimetype.3gp",
         mimetype="video/mpeg",
         version="2",
-        videomd=[{"codec": "mpeg4"}],
-        audiomd=[{"codec": "amrnb"}])
+        videomd=[
+            {"codec_name": "AVC",
+             "width": 176,
+             "height": 144,
+             "sample_aspect_ratio": "1:1",
+             "display_aspect_ratio": "11:9",
+             "avg_frame_rate": "15/1",
+             "duration": "4.933333",
+             "level": "0"}],
+        audiomd=[
+            {"codec": "amrnb",
+             "sample_rate": "8000",
+             "duration": "5.000000",
+             "channels": "1"}])
 
     check_ffmpeg_nok(
         filename="no_video.wav",
         mimetype="video/mpeg",
         version="1",
-        audiomd=[{"codec": "wav"}])
+        audiomd=[
+            {"codec_name": "wav",
+             "duration": "2.936625",
+             "channels": "2"}])
 
     check_ffmpeg_nok(
         filename="mpg1.mpg",
         mimetype="video/mpeg",
         version="1",
-        videomd=[{"codec": "mpeg1video"}, {"codec": "mpeg1video"}])
+        videomd=[
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': '-99',
+             'duration': '19.025400',
+             'width': '320',
+             'display_aspect_ratio': '4:3',
+             'height': '240',
+             'avg_frame_rate': '30000/1001'},
+            {'codec_name': 'MPEG 1',
+             'sample_aspect_ratio': '1:1',
+             'level': '-99',
+             'duration': '19.025400',
+             'width': '320',
+             'display_aspect_ratio': '4:3',
+             'height': '240',
+             'avg_frame_rate': '30000/1001'}])
 
     check_ffmpeg_nok(
         filename="mpg1.mpg",
