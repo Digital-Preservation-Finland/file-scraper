f18d301cc40048839513a491917303f76e8b571c
Mikko Vatanen <mikko.vatanen@csc.fi>
2016-12-22 12:47:33 +0200

    KDKPAS-1388 Delete temporary file after test, cleanup test code

--- tests/validator/plugin/csv_test.py
+++ tests/validator/plugin/csv_test.py
@@ -1,7 +1,6 @@
-"""Test for python CSV validation"""
-import os
-import pytest
+"""Tests for PythonCsv validator"""
 
+import os
 from tempfile import NamedTemporaryFile
 
 from ipt.validator.csv_validator import PythonCsv
@@ -13,140 +12,114 @@ VALID_CSV = (
     '''1997,Ford,E350,"ac, abs, moon",3000.00\n'''
     '''1999,Chevy,"Venture ""Extended Edition""","",4900.00\n'''
     '''1999,Chevy,"Venture ""Extended Edition, Very Large""",,5000.00\n'''
-    '''1996,Jeep,Grand Cherokee,"MUST SELL!\nair, moon roof, loaded",4799.00\n''')
+    '''1996,Jeep,Grand Cherokee,"MUST SELL!\n'''
+    '''air, moon roof, loaded",4799.00\n''')
 
 VALID_WITH_HEADER = \
     'year,brand,model,detail,other\n' + VALID_CSV
 
-
-INVALID_CSV = VALID_CSV + \
+MISSING_END_QUOTE = VALID_CSV + \
     '1999,Chevy,"Venture ""Extended Edition"","",4900.00\n'
 
+DEFAULT_FORMAT = {
+    "mimetype": "text/csv",
+    "version": "",
+    "charset": "UTF-8"}
 
-def write_testdata(infile, data=VALID_CSV):
+DEFAULT_ADDML = {
+    "charset": "UTF-8",
+    "separator": "CR+LF",
+    "delimiter": ";",
+    "header_fields": ""}
 
-    infile.write(data)
-    infile.close()
-    return infile.name
+
+def run_validator(csv_text, addml=None, file_format=None):
+    """Write test data and run csv validation for the file"""
+
+    if addml is None:
+        addml = DEFAULT_ADDML
+
+    if file_format is None:
+        file_format = DEFAULT_FORMAT
+
+    with NamedTemporaryFile(delete=False) as outfile:
+
+        try:
+            outfile.write(csv_text)
+            outfile.close()
+
+            fileinfo = {
+                "filename": outfile.name,
+                "format": file_format,
+                "addml": addml
+            }
+
+            validator = PythonCsv(fileinfo)
+            validator.validate()
+        finally:
+            os.unlink(outfile.name)
+
+    return validator
 
 
-def test_validate_success_wikipedia():
+def test_valid_no_header():
     """Test the validator with valid data from Wikipedia's CSV article"""
 
-    with NamedTemporaryFile(delete=False) as csv_file:
-        fileinfo = {
-            "filename": write_testdata(csv_file, VALID_CSV),
-            "format": {
-                "mimetype": "text/csv",
-                "version": "",
-                "charset": "UTF-8"
-            },
-            "addml": {
-                "charset": "UTF-8",
-                "separator": "CR+LF",
-                "delimiter": ";",
-                "header_fields": ""
-            }
-        }
+    validator = run_validator(VALID_CSV)
 
-        validator = PythonCsv(fileinfo)
-        validator.validate()
-
-        assert validator.is_valid, validator.messages() + validator.errors()
-        assert "CSV validation OK" in validator.messages()
-        assert validator.errors() == ""
-
-    with NamedTemporaryFile(delete=False) as csv_file:
-        fileinfo = {
-            "filename": write_testdata(csv_file, VALID_WITH_HEADER),
-            "format": {
-                "mimetype": "text/csv",
-                "version": "",
-                "charset": "UTF-8"
-            },
-            "addml": {
-                "charset": "UTF-8",
-                "separator": "CR+LF",
-                "delimiter": ";",
-                "header_fields": ["year", "brand", "model", "detail", "other"]
-            }
-        }
-
-        validator = PythonCsv(fileinfo)
-        validator.validate()
-
-        assert validator.is_valid
-        assert "CSV validation OK" in validator.messages()
-        assert validator.errors() == ""
+    assert validator.is_valid, validator.messages() + validator.errors()
+    assert "CSV validation OK" in validator.messages()
+    assert validator.errors() == ""
 
 
-def test_validate_failure():
+def test_valid_with_header():
+    """Test valid CSV with headers"""
 
-    with NamedTemporaryFile(delete=False) as csv_file:
-        fileinfo = {
-            "filename": write_testdata(csv_file, VALID_WITH_HEADER),
-            "format": {
-                "mimetype": "text/csv",
-                "version": "",
-                "charset": "UTF-8"
-            },
-            "addml": {
-                "charset": "UTF-8",
-                "separator": "CR+LF",
-                "delimiter": ";",
-                "header_fields": ["MISSING HEADER"]
-            }
-        }
+    addml = {
+        "charset": "UTF-8",
+        "separator": "CR+LF",
+        "delimiter": ";",
+        "header_fields": ["year", "brand", "model", "detail", "other"]
+    }
 
-        validator = PythonCsv(fileinfo)
-        validator.validate()
+    validator = run_validator(VALID_WITH_HEADER, addml)
 
-        assert not validator.is_valid
-        assert "CSV validation OK" not in validator.messages()
-        assert "no header at first line" in validator.errors()
+    assert validator.is_valid, validator.messages() + validator.errors()
+    assert "CSV validation OK" in validator.messages()
+    assert validator.errors() == ""
 
-    with NamedTemporaryFile(delete=False) as csv_file:
-        fileinfo = {
-            "filename": PDF_PATH,
-            "format": {
-                "mimetype": "text/csv",
-                "version": "",
-                "charset": "UTF-8"
-            },
-            "addml": {
-                "charset": "UTF-8",
-                "separator": "CR+LF",
-                "delimiter": ";",
-                "header_fields": ""
-            }
-        }
 
-        validator = PythonCsv(fileinfo)
-        validator.validate()
+def test_missing_header():
+    """Test in invalid csv validation"""
 
-        assert not validator.is_valid
-        assert len(validator.errors()) != 0
-        assert "CSV validation OK" not in validator.messages(), validator.messages() + validator.errors()
+    addml = {
+        "charset": "UTF-8",
+        "separator": "CR+LF",
+        "delimiter": ";",
+        "header_fields": ["MISSING HEADER"]}
 
-    with NamedTemporaryFile(delete=False) as csv_file:
-        fileinfo = {
-            "filename": write_testdata(csv_file, INVALID_CSV),
-            "format": {
-                "mimetype": "text/csv",
-                "version": "",
-                "charset": "UTF-8"
-            },
-            "addml": {
-                "charset": "UTF-8",
-                "separator": "CR+LF",
-                "delimiter": ";",
-                "header_fields": ""
-            }
-        }
+    validator = run_validator(VALID_WITH_HEADER, addml)
 
-        validator = PythonCsv(fileinfo)
-        validator.validate()
+    assert not validator.is_valid, validator.messages() + validator.errors()
+    assert "CSV validation OK" not in validator.messages()
+    assert "no header at first line" in validator.errors()
 
-        assert not validator.is_valid
-        assert "CSV validation OK" not in validator.messages()
-        assert len(validator.errors())
+
+def test_pdf_as_csv():
+    """Test CSV validator with PDF files"""
+
+    validator = run_validator(open(PDF_PATH).read())
+
+    assert not validator.is_valid, validator.messages() + validator.errors()
+    assert "CSV validation OK" not in validator.messages()
+    assert len(validator.errors()) > 0
+
+
+def test_missing_end_quote():
+    """Test missing end quote"""
+
+    validator = run_validator(MISSING_END_QUOTE)
+
+    assert not validator.is_valid, validator.messages() + validator.errors()
+    assert "CSV validation OK" not in validator.messages()
+    assert len(validator.errors()) > 0
