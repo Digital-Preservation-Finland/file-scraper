56942d202656b207b8ba0881d1bffafe89884c0e
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-02-14 14:42:20 +0200

    KDKPAS-1195: PEP8 cleanup

--- ipt/validator/office.py
+++ ipt/validator/office.py
@@ -13,25 +13,24 @@ class Office(BaseValidator):
     """
     Office valiator
     """
-    _supported_mimetypes = {\
-            'application/vnd.oasis.opendocument.text': ['1.0', '1.1', '1.2'],
-            'application/vnd.oasis.opendocument.spreadsheet':\
-                    ['1.0', '1.1', '1.2'],
-            'application/vnd.oasis.opendocument.presentation':\
-                    ['1.0', '1.1', '1.2'],
-            'application/vnd.oasis.opendocument.graphics':\
-                    ['1.0', '1.1', '1.2'],
-            'application/vnd.oasis.opendocument.formula': ['1.0', '1.1', '1.2'],
-            'application/msword': ['8.0', '8.5', '9.0', '10.0', '11.0'],
-            'application/vnd.ms-excel': ['8.0', '9.0', '10.0', '11.0'],
-            'application/vnd.ms-powerpoint': ['8.0', '9.0', '10.0', '11.0'],
-            'application/vnd.openxmlformats-officedocument.wordprocessingml.'+\
-                    'document': ['12.0', '14.0', '15.0'],
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.'+\
-                    'sheet': ['12.0', '14.0', '15.0'],
-            'application/vnd.openxmlformats-officedocument.presentationml.'+\
-                    'presentation': ['12.0', '14.0', '15.0']\
-            }
+    _supported_mimetypes = {
+        'application/vnd.oasis.opendocument.text': ['1.0', '1.1', '1.2'],
+        'application/vnd.oasis.opendocument.spreadsheet':
+            ['1.0', '1.1', '1.2'],
+        'application/vnd.oasis.opendocument.presentation':
+            ['1.0', '1.1', '1.2'],
+        'application/vnd.oasis.opendocument.graphics': ['1.0', '1.1', '1.2'],
+        'application/vnd.oasis.opendocument.formula': ['1.0', '1.1', '1.2'],
+        'application/msword': ['8.0', '8.5', '9.0', '10.0', '11.0'],
+        'application/vnd.ms-excel': ['8.0', '9.0', '10.0', '11.0'],
+        'application/vnd.ms-powerpoint': ['8.0', '9.0', '10.0', '11.0'],
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.'+
+        'document': ['12.0', '14.0', '15.0'],
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            ['12.0', '14.0', '15.0'],
+        'application/vnd.openxmlformats-officedocument.presentationml.'+
+        'presentation': ['12.0', '14.0', '15.0']
+    }
 
 
     def validate(self):
--- tests/validator/plugin/office_test.py
+++ tests/validator/plugin/office_test.py
@@ -10,34 +10,32 @@ from ipt.validator.office import Office
 BASEPATH = "tests/data/02_filevalidation_data/office"
 
 
-@pytest.mark.parametrize(\
-        ['filename', 'mimetype', 'version'],
-        [
-            ("ODF_Text_Document.odt",\
-                    "application/vnd.oasis.opendocument.text", "1.1"),
-            ("ODF_Text_Document.odt",\
-                    "application/vnd.oasis.opendocument.text", "1.2"),
-            ("MS_Word_97-2003.doc", "application/msword", "11.0"),
-            ("Office_Open_XML_Text.docx", "application/vnd.openxmlformats-"+\
-                    "officedocument.wordprocessingml.document", "15.0"),
-            ("ODF_Presentation.odp",\
-                    "application/vnd.oasis.opendocument.presentation", "1.2"),
-            ("MS_PowerPoint_97-2003.ppt", "application/vnd.ms-powerpoint",\
-                "11.0"),
-            ("Office_Open_XML_Presentation.pptx", "application/vnd.openxml"+\
-                    "formats-officedocument.presentationml.presentation",\
-                    "15.0"),
-            ("ODF_Spreadsheet.ods",\
-                "application/vnd.oasis.opendocument.spreadsheet", "1.2"),
-            ("MS_Excel_97-2003.xls", "application/vnd.ms-excel", "11.0"),
-            ("Excel_Online_Spreadsheet.xlsx", "application/vnd."+\
-                    "openxmlformats-officedocument.spreadsheetml.sheet",\
-                    "15.0"),
-            ("ODF_Drawing.odg", "application/vnd.oasis.opendocument.graphics",\
-                "1.2"),
-            ("ODF_Formula.odf", "application/vnd.oasis.opendocument.formula",\
-                "1.2"),
-        ]\
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        ("ODF_Text_Document.odt", "application/vnd.oasis.opendocument.text",
+         "1.1"),
+        ("ODF_Text_Document.odt", "application/vnd.oasis.opendocument.text",
+         "1.2"),
+        ("MS_Word_97-2003.doc", "application/msword", "11.0"),
+        ("Office_Open_XML_Text.docx", "application/vnd.openxmlformats-"+
+         "officedocument.wordprocessingml.document", "15.0"),
+        ("ODF_Presentation.odp",
+         "application/vnd.oasis.opendocument.presentation", "1.2"),
+        ("MS_PowerPoint_97-2003.ppt", "application/vnd.ms-powerpoint", "11.0"),
+        ("Office_Open_XML_Presentation.pptx", "application/vnd.openxml"+
+         "formats-officedocument.presentationml.presentation", "15.0"),
+        ("ODF_Spreadsheet.ods",
+         "application/vnd.oasis.opendocument.spreadsheet", "1.2"),
+        ("MS_Excel_97-2003.xls", "application/vnd.ms-excel", "11.0"),
+        ("Excel_Online_Spreadsheet.xlsx",
+         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+         "15.0"),
+        ("ODF_Drawing.odg", "application/vnd.oasis.opendocument.graphics",
+         "1.2"),
+        ("ODF_Formula.odf", "application/vnd.oasis.opendocument.formula",
+         "1.2"),
+    ]
 )
 
 
@@ -56,25 +54,23 @@ def test_validate_valid_file(filename, mimetype, version):
     assert validator.is_valid
 
 
-@pytest.mark.parametrize(\
-        ['filename', 'mimetype', 'version'],\
-        [\
-            ("Office_Open_XML_Spreadsheet.xlsx", "application/vnd."+\
-                    "openxmlformats-officedocument.spreadsheetml.sheet",\
-                    "15.0"),
-            ("empty_file.doc", "application/msword", "11.0"),
-            ("ODF_Text_Document.odt", "application/msword", "11.0"),
-            ("ODF_Text_Document_corrupted.odt",\
-                    "application/vnd.oasis.opendocument.text", "1.2"),
-            ("ODF_Text_Document_with_wrong_filename_extension.doc",\
-                "application/msword", "11.0"),
-            ("MS_Word_2007-2013_XML_zip.docx", "application/vnd."+\
-                    "openxmlformats-officedocument.wordprocessingml.document",\
-                    "15.0"),
-            ("MS_Word_97-2003.doc", "application/msword", "15.0"),
-            ("ODF_Text_Document.odt",\
-                "application/vnd.oasis.opendocument.text", ""),
-        ]\
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        ("Office_Open_XML_Spreadsheet.xlsx", "application/vnd."+
+         "openxmlformats-officedocument.spreadsheetml.sheet", "15.0"),
+        ("empty_file.doc", "application/msword", "11.0"),
+        ("ODF_Text_Document.odt", "application/msword", "11.0"),
+        ("ODF_Text_Document_corrupted.odt",
+         "application/vnd.oasis.opendocument.text", "1.2"),
+        ("ODF_Text_Document_with_wrong_filename_extension.doc",
+         "application/msword", "11.0"),
+        ("MS_Word_2007-2013_XML_zip.docx", "application/vnd.openxmlformats-"+
+         "officedocument.wordprocessingml.document", "15.0"),
+        ("MS_Word_97-2003.doc", "application/msword", "15.0"),
+        ("ODF_Text_Document.odt",
+         "application/vnd.oasis.opendocument.text", ""),
+    ]
 )
 
 
