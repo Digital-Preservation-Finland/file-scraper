fbacc1cf4f0f1d6ac1e3dfe285417359a6537e96
Mikko Vatanen <mikko.vatanen@csc.fi>
2017-02-27 17:47:44 +0200

    KDKPAS-1195 Test for exact number of validator plugins

--- tests/validator/utils_test.py
+++ tests/validator/utils_test.py
@@ -2,7 +2,7 @@
 that all validators are able to be found."""
 
 from ipt.validator.jhove import JHovePDF, \
-    JHoveTiff, JHoveJPEG, JHoveHTML, JHoveGif
+    JHoveTiff, JHoveJPEG, JHoveHTML, JHoveGif, JHoveTextUTF8
 from ipt.validator.dummytextvalidator import DummyTextValidator
 from ipt.validator.xmllint import Xmllint
 from ipt.validator.warctools import WarctoolsWARC, WarctoolsARC
@@ -18,32 +18,32 @@ import pytest
 @pytest.mark.parametrize(
     ["mimetype", "version", "charset", "validator_class"],
     [
-        ("application/warc", "1.0", "", WarctoolsWARC),
-        ("text/csv", "", "UTF-8", PythonCsv),
-        ("text/plain", "", "ISO-8859-15", DummyTextValidator),
-        ("video/mpeg", "1", "", FFMpeg),
-        ("video/mpeg", "2", "", FFMpeg),
-        ("video/mp4", "", "", FFMpeg),
-        ("application/pdf", "1.3", "", JHovePDF),
-        ("application/pdf", "1.4", "", JHovePDF),
-        ("application/pdf", "1.5", "", JHovePDF),
-        ("application/pdf", "1.5", "", JHovePDF),
-        ("application/pdf", "1.6", "", JHovePDF),
-        ("application/pdf", "A-1a", "", JHovePDF),
-        ("application/pdf", "A-1b", "", JHovePDF),
-        ("application/pdf", "1.7", "", GhostScript),
-        ("image/tiff", "6.0", "", JHoveTiff),
-        ("image/jpeg", "", "", JHoveJPEG),
-        ("image/gif", "1987a", "", JHoveGif),
-        ("image/gif", "1989a", "", JHoveGif),
-        ("text/html", "HTML.4.01", "UTF-8", JHoveHTML),
-        ("image/png", "", "", Pngcheck),
-        ("application/warc", "0.17", "", WarctoolsWARC),
-        ("application/warc", "0.18", "", WarctoolsWARC),
-        ("application/warc", "1.0", "", WarctoolsWARC),
-        ("application/x-internet-archive", "1.0", "", WarctoolsARC),
-        ("application/x-internet-archive", "1.1", "", WarctoolsARC),
-        ("text/xml", "1.0", "UTF-8", Xmllint)
+        ("application/warc", "1.0", "", [WarctoolsWARC]),
+        ("text/csv", "", "UTF-8", [PythonCsv]),
+        ("text/plain", "", "ISO-8859-15", [DummyTextValidator, JHoveTextUTF8]),
+        ("video/mpeg", "1", "", [FFMpeg]),
+        ("video/mpeg", "2", "", [FFMpeg]),
+        ("video/mp4", "", "", [FFMpeg]),
+        ("application/pdf", "1.3", "", [JHovePDF]),
+        ("application/pdf", "1.4", "", [JHovePDF]),
+        ("application/pdf", "1.5", "", [JHovePDF]),
+        ("application/pdf", "1.5", "", [JHovePDF]),
+        ("application/pdf", "1.6", "", [JHovePDF]),
+        ("application/pdf", "A-1a", "", [JHovePDF]),
+        ("application/pdf", "A-1b", "", [JHovePDF]),
+        ("application/pdf", "1.7", "", [GhostScript]),
+        ("image/tiff", "6.0", "", [JHoveTiff]),
+        ("image/jpeg", "", "", [JHoveJPEG]),
+        ("image/gif", "1987a", "", [JHoveGif]),
+        ("image/gif", "1989a", "", [JHoveGif]),
+        ("text/html", "HTML.4.01", "UTF-8", [JHoveHTML]),
+        ("image/png", "", "", [Pngcheck]),
+        ("application/warc", "0.17", "", [WarctoolsWARC]),
+        ("application/warc", "0.18", "", [WarctoolsWARC]),
+        ("application/warc", "1.0", "", [WarctoolsWARC]),
+        ("application/x-internet-archive", "1.0", "", [WarctoolsARC]),
+        ("application/x-internet-archive", "1.1", "", [WarctoolsARC]),
+        ("text/xml", "1.0", "UTF-8", [Xmllint])
     ])
 def tests_iter_validator_classes(mimetype, version, charset, validator_class):
     """
@@ -64,4 +64,4 @@ def tests_iter_validator_classes(mimetype, version, charset, validator_class):
         }
     }
     validators = iter_validators(fileinfo)
-    assert any([isinstance(x, validator_class) for x in validators])
+    assert [type(x) for x in validators] == validator_class
