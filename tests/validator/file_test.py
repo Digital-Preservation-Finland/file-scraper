f6fc5025b754c75532910ffe66fe610b2befd39a
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2017-02-28 10:49:32 +0200

    KDKPAS-1195: Add tests for File-validator

---
+++ tests/validator/file_test.py
@@ -0,0 +1,89 @@
+"""
+Tests for File (libmagick) validator.
+"""
+
+import os
+import pytest
+from ipt.validator.file import File
+
+
+BASEPATH = "tests/data/02_filevalidation_data/office"
+
+
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        ("ODF_Text_Document.odt", "application/vnd.oasis.opendocument.text",
+         "1.1"),
+        ("ODF_Text_Document.odt", "application/vnd.oasis.opendocument.text",
+         "1.2"),
+        ("MS_Word_97-2003.doc", "application/msword", "11.0"),
+        ("Office_Open_XML_Text.docx", "application/vnd.openxmlformats-"
+         "officedocument.wordprocessingml.document", "15.0"),
+        ("ODF_Presentation.odp",
+         "application/vnd.oasis.opendocument.presentation", "1.2"),
+        ("MS_PowerPoint_97-2003.ppt", "application/vnd.ms-powerpoint", "11.0"),
+        ("Office_Open_XML_Presentation.pptx", "application/vnd.openxml"
+         "formats-officedocument.presentationml.presentation", "15.0"),
+        ("ODF_Spreadsheet.ods",
+         "application/vnd.oasis.opendocument.spreadsheet", "1.2"),
+        ("MS_Excel_97-2003.xls", "application/vnd.ms-excel", "11.0"),
+        ("Excel_Online_Spreadsheet.xlsx",
+         "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+         "15.0"),
+        ("ODF_Drawing.odg", "application/vnd.oasis.opendocument.graphics",
+         "1.2"),
+        ("ODF_Formula.odf", "application/vnd.oasis.opendocument.formula",
+         "1.2"),
+    ]
+)
+
+
+def test_validate_valid_file(filename, mimetype, version):
+
+    fileinfo = {
+        'filename': os.path.join(BASEPATH, filename),
+        'format': {
+            'mimetype': mimetype,
+            'version': version
+        }
+    }
+
+    validator = File(fileinfo)
+    validator.validate()
+    assert validator.is_valid
+
+
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        # Empty file
+        ("empty_file.doc", "application/msword", "11.0"),
+        # .zip renamed to .docx
+        ("MS_Word_2007-2013_XML_zip.docx", "application/vnd.openxmlformats-"
+         "officedocument.wordprocessingml.document", "15.0"),
+        # Bad xmlx created by LibreOffice
+        ("Office_Open_XML_Spreadsheet.xlsx", "application/vnd."
+         "openxmlformats-officedocument.spreadsheetml.sheet", "15.0"),
+        # Wrong MIME
+        ("ODF_Text_Document.odt", "application/msword", "11.0"),
+        # .odt renamed to .doc
+        ("ODF_Text_Document_with_wrong_filename_extension.doc",
+         "application/msword", "11.0"),
+    ]
+)
+
+
+def test_validate_invalid_file(filename, mimetype, version):
+
+    fileinfo = {
+        'filename': os.path.join(BASEPATH, filename),
+        'format': {
+            'mimetype': mimetype,
+            'version': version
+        }
+    }
+
+    validator = File(fileinfo)
+    validator.validate()
+    assert not validator.is_valid
---
+++ tests/validator/utils_test_multiple_validators.py
@@ -0,0 +1,68 @@
+"""
+Tests office-file validation with combination of Office-validator and
+File-validator.
+"""
+
+import os
+import pytest
+from ipt.validator.utils import iter_validators
+from ipt.validator.office import Office
+from ipt.validator.file import File
+
+
+BASEPATH = "tests/data/02_filevalidation_data/office"
+
+
+# Test valid file
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        ("ODF_Text_Document.odt", "application/vnd.oasis.opendocument.text",
+         "1.2"),
+    ]
+)
+
+def test_validate_valid_file(filename, mimetype, version):
+
+    fileinfo = {
+        'filename': os.path.join(BASEPATH, filename),
+        'format': {
+            'mimetype': mimetype,
+            'version': version
+        }
+    }
+
+    for validator in iter_validators(fileinfo):
+        assert validator.result()['is_valid']
+
+
+# Test invalid files
+@pytest.mark.parametrize(
+    ['filename', 'mimetype', 'version'],
+    [
+        # Corrupted file - caught by Office validator
+        ("ODF_Text_Document_corrupted.odt",
+         "application/vnd.oasis.opendocument.text", "1.2"),
+        # Wrong MIME - caught by File validator
+        ("ODF_Text_Document.odt", "application/msword", "11.0"),
+        # Unsupported version number - validator not found
+        ("MS_Word_97-2003.doc", "application/msword", "15.0"),
+    ]
+)
+
+def test_validate_invalid_file(filename, mimetype, version):
+
+    fileinfo = {
+        'filename': os.path.join(BASEPATH, filename),
+        'format': {
+            'mimetype': mimetype,
+            'version': version
+        }
+    }
+
+    validator_results = []
+    for validator in iter_validators(fileinfo):
+        validator_results.append(validator.result()['is_valid'])
+
+    assert not all(validator_results)
+    assert len(validator_results) > 0
