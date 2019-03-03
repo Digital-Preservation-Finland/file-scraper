894ca4fffcb8ea9e3330f77cf06fe6d82a074a54
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-04 00:53:38 +0200

    KDKPAS-1899 Modify dpx validator to dpx scraper.

--- dpres_scraper/scrapers/dpx.py
+++ dpres_scraper/scrapers/dpx.py
@@ -1,36 +1,33 @@
 """
-This is an DPX V2.0 validator
+This is an DPX V2.0 scraper
 """
+from dpres_scraper.base import BaseScraper, Shell
 
-from ipt.validator.basevalidator import BaseValidator, Shell
 
-
-class DPXv(BaseValidator):
+class Dpx(BaseScraper):
+    """DPX scraper
     """
-    DPX validator
-    """
-    _supported_mimetypes = {
-        "image/x-dpx": ["2.0"]
-    }
 
-    def validate(self):
-        """
-        Validate file and version by passing the version from the METS
-        metadata_info data as a flavour argument to the validator which
-        selects a specific validation profile for the stated version.
-        The validator returns an XML validation report to stdout. If the
-        file is not valid or the version is not compliant with the
-        selected validation profile the validation report is sent to
-        stderr.
-        """
+    _supported = {'image/x-dpx': []}
+    _only_wellformed = True
 
-        shell = Shell(['dpxv', self.metadata_info['filename']])
+    def scrape_file(self):
+        """Scrape DPX
+        """
+        shell = Shell(['dpxv', self.filename])
 
         if shell.returncode != 0:
             raise DPXvError(shell.stderr)
 
         self.errors(shell.stderr)
         self.messages(shell.stdout)
+        self._collect_elements()
+
+    # pylint: disable=no-self-use
+    def _s_stream_type(self):
+        """Return file type
+        """
+        return 'image'
 
 
 class DPXvError(Exception):
