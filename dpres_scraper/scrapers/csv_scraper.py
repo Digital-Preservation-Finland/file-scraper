3ad238e6999a69a4887a144940462e28fa852962
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-04 00:53:21 +0200

    KDKPAS-1899 Modify csv validator to csv scraper.

--- dpres_scraper/scrapers/csv_scraper.py
+++ dpres_scraper/scrapers/csv_scraper.py
@@ -1,67 +1,57 @@
-"""The CSV validator plugin"""
+"""Scraper for CSV file formats
+"""
 import csv
 
-from ipt.validator.basevalidator import BaseValidator
+from dpres_scraper.base import BaseScraper
 
 
-class CsvValidationError(Exception):
-    """CSV validation error"""
-    pass
+class Csv(BaseScraper):
+    """Scraper for CSV files
+    """
 
+    _supported = {'text/csv': []}
 
-class PythonCsv(BaseValidator):
-    """The CSV validator plugin"""
-
-    _supported_mimetypes = {
-        'text/csv': [''],
-    }
-
-    def __init__(self, metadata_info):
-        super(PythonCsv, self).__init__(metadata_info)
-
-        self.filename = metadata_info['filename']
-
-        if "addml" in metadata_info:
-            self.charset = metadata_info['addml']['charset']
-            self.record_separator = metadata_info['addml']['separator']
-            self.delimiter = metadata_info['addml']['delimiter']
-            self.header_fields = metadata_info['addml']['header_fields']
-
-    def validate(self):
-        """Try to read CSV file through cvs.reader and if that can be done file
-        is valid.
-        :returns: (statuscode, messages, errors)
+    def __init__(self, mimetype, filename, validation):
+        """Initialize for delimiter and separator info
         """
-        # TODO: This issue involves all validators that use ADDML
-        # Fix this issue more generically if it becomes more widespread
-        if "addml" not in self.metadata_info:
-            self.errors("ADDML data was expected, but not found")
-            return
+        self._csv_delimiter = None
+        self._csv_separator = None
+        super(Csv, self).__init__(mimetype, filename, validation)
 
-        class _Dialect(csv.excel):
-            """Init dialect, example from Python csv.py library"""
-            strict = True
-            delimiter = self.delimiter
-            doublequote = True
-            lineterminator = self.record_separator
-
+    def scrape_file(self):
+        """Scrape CSV file
+        """
         try:
             with open(self.filename, 'rb') as csvfile:
-                reader = csv.reader(csvfile, _Dialect)
+                reader = csv.reader(csvfile)
                 first_line = reader.next()
-
-                if len(self.header_fields) > 0 and \
-                        len(self.header_fields) != len(first_line):
-                    self.errors(
-                        "CSV validation error: field counts in the ADDML "
-                        "document and the CSV header don't match"
-                    )
-                    return
+                sniffer = csv.Sniffer()
+                dialect = sniffer.sniff(first_line)
+                self._csv_delimiter = dialect.delimiter
+                self._csv_separator = dialect.lineterminator
                 for _ in reader:
                     pass
-
         except csv.Error as exception:
-            self.errors("CSV validation error on line %s: %s" %
+            self.errors("CSV error on line %s: %s" %
                         (reader.line_num, exception))
-            return
-        self.messages("CSV validation OK")
+        else:
+            self.messages("File was scraped successfully.")
+        finally:
+            self._collect_elements()
+
+
+    def _s_delimiter(self):
+        """Return delimiter
+        """
+        return self._csv_delimiter
+
+    def _s_separator(self):
+        """Return separator
+        """
+        return self._csv_separator
+
+    # pylint: disable=no-self-use
+    def _s_stream_type(self):
+        """Return file type
+        """
+        return 'char'
