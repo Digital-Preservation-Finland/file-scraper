f5ced2df6fae0bfebc3c2c18c755c775256b8dd5
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-04 00:43:34 +0200

    KDKPAS-1899 Modify imagemagick validator to wand base scraper.

--- dpres_scraper/wand_base.py
+++ dpres_scraper/wand_base.py
@@ -1,55 +1,117 @@
-"""
-This is an ImageMagick validator.
-"""
-
-
-from ipt.validator.basevalidator import BaseValidator
+"""Metadata scraper for image file formats"""
 try:
-    from wand.image import Image
+    import wand.image
 except ImportError:
-    WAND_IMPORT = False
-else:
-    WAND_IMPORT = True
+    pass
+
+from dpres_scraper.base import BaseScraper
 
 
-FORMAT_STRINGS = {
-    'image/png': 'PNG',
-    'image/jpeg': 'JPEG',
-    'image/jp2': 'JP2',
-    'image/tiff': 'TIFF',
-    }
-
-
-class ImageMagick(BaseValidator):
+class Wand(BaseScraper):
+    """Scraper class for collecting image metadata
     """
-    ImageMagick validator
-    """
-    _supported_mimetypes = {
-        "image/png": ["1.2"],
-        "image/jpeg": ["1.00", "1.01", "1.02"],
-        "image/jp2": [""],
-        "image/tiff": ["6.0"],
-    }
 
-    def validate(self):
+    def __init__(self, mimetype, filename, validation):
+        """Initialize scraper
         """
-        Validate file
-        """
-        if not WAND_IMPORT:
-            self.errors("Unable to load Wand library for Python. "
-                        "The ImageMagicK library might be missing.")
-            return
+        self._wand_index = None
+        self._wand_stream = None
+        self._wand = None
+        super(Wand, self).__init__(mimetype, filename, validation)
 
+    def scrape_file(self):
+        """Scrape data from file
+        """
         try:
-            img = Image(filename=self.metadata_info['filename'])
+            self._wand = wand.image.Image(filename=self.filename)
         except:
-            self.errors("Could not read image")
+            self.errors('Error in scraping file.')
         else:
-            format_name = img.format
-            if img.format == FORMAT_STRINGS[
-                    self.metadata_info['format']['mimetype']]:
-                self.messages("ImageMagick detected format: " + format_name)
-            else:
-                self.errors("File format does not match with MIME type.")
-            # ensure that temporary files are not left in /tmp/
-            img.__del__()
+            self.messages('The file was scraped.')
+        finally:
+            self._collect_elements()
+
+    def iter_tool_streams(self, stream_type):
+        """Iterate image streams
+        """
+        if stream_type in [None, 'image']:
+            for index, stream in enumerate(self._wand.sequence):
+                self._wand_stream = stream
+                self._wand_index = index
+                yield stream
+
+    def set_tool_stream(self, index):
+        """Set image stream
+        """
+        self._wand_index = index
+        self._wand_stream = self._wand.sequence[index]
+
+    # pylint: disable=no-self-use
+    def _s_version(self):
+        """Return version of file
+        """
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_stream_type(self):
+        """Return stream type
+        """
+        return 'image'
+
+    def _s_index(self):
+        """Return stream index
+        """
+        return self._wand_index
+
+    def _s_colorspace(self):
+        """Returns colorspace
+        """
+        if self._wand_stream.colorspace is not None:
+            return self._wand_stream.colorspace
+        return None
+
+    def _s_width(self):
+        """Returns image width
+        """
+        if self._wand_stream.width is not None:
+            return str(self._wand_stream.width)
+        return None
+
+    def _s_height(self):
+        """Returns image height
+        """
+        if self._wand_stream.height is not None:
+            return str(self._wand_stream.height)
+        return None
+
+    def _s_bps_value(self):
+        """Returns bits per sample
+        """
+        if self._wand_stream.depth is not None:
+            return str(self._wand_stream.depth)
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_bps_unit(self):
+        """Returns sample unit
+        """
+        return None
+
+    def _s_compression(self):
+        """Returns compression scheme
+        """
+        if self._wand.compression is not None:
+            return self._wand.compression
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_byte_order(self):
+        """Returns byte order
+        """
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_samples_per_pixel(self):
+        """Returns samples per pixel
+        """
+        return None
