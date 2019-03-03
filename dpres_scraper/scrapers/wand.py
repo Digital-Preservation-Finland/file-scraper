a6eba3702f160867373ed32b65ff7bbffcaeeee7
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-04 01:08:22 +0200

    KDKPAS-1899 Add wand scraper.

---
+++ dpres_scraper/scrapers/wand.py
@@ -0,0 +1,27 @@
+"""Metadata scraper for image file formats"""
+from dpres_scraper.wand_base import Wand
+
+
+class TiffWand(Wand):
+    """Collect TIFF image metadata
+    """
+
+    _supported = {'image/tiff': []}
+
+    def _s_byte_order(self):
+        """Returns byte order
+        """
+        for key, value in self._wand.metadata.items():
+            if key.startswith('tiff:endian'):
+                if value == 'msb':
+                    return 'big endian'
+                else:
+                    return 'little endian'
+
+
+class ImageWand(Wand):
+    """Collect image metadata
+    """
+
+    _supported = {'image/png': [], 'image/jpeg': [], 'image/jp2': [],
+                  'image/x-dpx': []}
