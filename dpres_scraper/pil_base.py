a9688f133f4ce022ad9a6d61fce7e74b46b211fb
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-04 00:46:36 +0200

    KDKPAS-1899 Add pil base scraper.

---
+++ dpres_scraper/pil_base.py
@@ -0,0 +1,120 @@
+"""Metadata scraper for image file formats"""
+try:
+    import PIL.Image
+except ImportError:
+    pass
+
+from dpres_scraper.base import BaseScraper
+
+
+SAMPLES_PER_PIXEL = {'1': '1', 'L': '1', 'P': '1', 'RGB': '3', 'YCbCr': '3',
+                     'LAB': '3', 'HSV': '3', 'RGBA': '4', 'CMYK': '4',
+                     'I': '1', 'F': '1'}
+
+
+class Pil(BaseScraper):
+    """Scraper class for collecting image metadata
+    """
+
+    def __init__(self, mimetype, filename, validation):
+        """Initialize scraper
+        """
+        self._pil = None
+        self._pil_index = None
+        super(Pil, self).__init__(mimetype, filename, validation)
+
+    def scrape_file(self):
+        """Scrape data from file
+        """
+        try:
+            self._pil = PIL.Image.open(self.filename)
+        except:
+            self.errors('Error in scraping file.')
+        else:
+            self.messages('The file was scraped.')
+        finally:
+            self._collect_elements()
+
+    def iter_tool_streams(self, stream_type):
+        """Iterate image streams
+        """
+        if stream_type in [None, 'image']:
+            if not hasattr(self._pil, 'n_frames'):
+                self._pil_index = 0
+                yield self._pil
+            else:
+                for index in range(0, self._pil.n_frames):
+                    self._pil.seek(index)
+                    self._pil_index = index
+                    yield self._pil
+
+    def set_tool_stream(self, index):
+        """Set image stream
+        """
+        self._pil_index = index
+        self._pil.seek(index)
+
+    # pylint: disable=no-self-use
+    def _s_version(self):
+        """Return version of file
+        """
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_stream_type(self):
+        """Return stream type
+        """
+        return 'image'
+
+    def _s_index(self):
+        """Return stream index
+        """
+        return self._pil_index
+
+    def _s_colorspace(self):
+        """Returns colorspace
+        """
+        return None
+
+    def _s_width(self):
+        """Returns image width
+        """
+        if self._pil.width is not None:
+            return str(self._pil.width)
+        return None
+
+    def _s_height(self):
+        """Returns image height
+        """
+        if self._pil.height is not None:
+            return str(self._pil.height)
+        return None
+
+    def _s_bps_value(self):
+        """Returns bits per sample
+        """
+        return None
+
+    def _s_bps_unit(self):
+        """Returns sample unit
+        """
+        if self._pil.mode == 'F':
+            return 'floating point'
+        else:
+            return 'integer'
+
+    def _s_compression(self):
+        """Returns compression scheme
+        """
+        return None
+
+    # pylint: disable=no-self-use
+    def _s_byte_order(self):
+        """Returns byte order
+        """
+        return None
+
+    def _s_samples_per_pixel(self):
+        """Returns samples per pixel
+        """
+        return SAMPLES_PER_PIXEL[self._pil.mode]
