d1393a25c1f83a72602248a2313247c3efde34b3
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-16 15:20:39 +0200

    KDKPAS-1899 Add invalid cases to vnu tests.

--- tests/scrapers/vnu_test.py
+++ tests/scrapers/vnu_test.py
@@ -15,7 +15,19 @@ MIMETYPE = 'text/html'
         ('valid_5.0.html', {
             'purpose': 'Test valid file.',
             'stdout_part': 'valid_5.0.html',
-            'stderr_part': ''})
+            'stderr_part': ''}),
+        ('invalid_5.0_nodoctype.html', {
+            'purpose': 'Test valid file.',
+            'stdout_part': '',
+            'stderr_part': 'Start tag seen without seeing a doctype first.'}),
+        ('invalid_5.0_illegal_tags.html', {
+            'purpose': 'Test valid file.',
+            'stdout_part': '',
+            'stderr_part': 'not allowed as child of element'}),
+        ('invalid__empty.html', {
+            'purpose': 'Test valid file.',
+            'stdout_part': '',
+            'stderr_part': 'End of file seen without seeing a doctype first'}),
     ]
 )
 def test_scraper(filename, result_dict):
@@ -25,6 +37,8 @@ def test_scraper(filename, result_dict):
     scraper = Vnu(correct.filename, correct.mimetype,
                   True, correct.params)
     scraper.scrape_file()
+    correct.version = '5.0'
+    correct.streams[0]['version'] = '5.0'
 
     assert scraper.mimetype == correct.mimetype
     assert scraper.version == correct.version
@@ -33,6 +47,7 @@ def test_scraper(filename, result_dict):
     assert correct.stdout_part in scraper.messages()
     assert correct.stderr_part in scraper.errors()
     assert scraper.well_formed == correct.well_formed
+
 
 def test_is_supported():
     """Test is_Supported method"""
