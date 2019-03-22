5950fb42211314d54df793a8c8d09b32b8709702
Juha Lehtonen <juha.lehtonen@csc.fi>
2019-03-22 17:49:00 +0200

    KDKPAS-1899 Simplify e2e test code.

--- tests/end_to_end_test.py
+++ tests/end_to_end_test.py
@@ -9,22 +9,25 @@ from tests.common import get_files
 # second and third streams, but exists in the first one.
 # MPEG-TS file contains "menu" stream, where version is None.
 NONE_ELEMENTS = {
-    'tests/data/application_msword/valid_11.0.doc': 'version',
-    'tests/data/application_vnd.ms-excel/valid_11.0.xls': 'version',
-    'tests/data/application_vnd.ms-powerpoint/valid_11.0.ppt': 'version',
+    'tests/data/application_x-internet-archive/valid_1.0_.arc.gz': ['version'],
+    'tests/data/application_msword/valid_11.0.doc': ['version'],
+    'tests/data/application_vnd.ms-excel/valid_11.0.xls': ['version'],
+    'tests/data/application_vnd.ms-powerpoint/valid_11.0.ppt': ['version'],
     'tests/data/application_vnd.oasis.opendocument.formula/valid_1.0'
-    '.odf': 'version',
+    '.odf': ['version'],
     'tests/data/application_vnd.openxmlformats-officedocument.presentationml'
-    '.presentation/valid_15.0.pptx': 'version',
+    '.presentation/valid_15.0.pptx': ['version'],
     'tests/data/application_vnd.openxmlformats-officedocument.spreadsheetml'
-    '.sheet/valid_15.0.xlsx': 'version',
+    '.sheet/valid_15.0.xlsx': ['version'],
     'tests/data/application_vnd.openxmlformats-officedocument.word'
-    'processingml.document/valid_15.0.docx': 'version',
-    'tests/data/image_gif/valid_1989a.gif': 'version, version',
-    'tests/data/image_tiff/valid_6.0_multiple_tiffs.tif': 'version, version',
-    'tests/data/video_MP2T/valid_.ts': 'version'}
+    'processingml.document/valid_15.0.docx': ['version'],
+    'tests/data/image_gif/valid_1989a.gif': ['version', 'version'],
+    'tests/data/image_tiff/valid_6.0_multiple_tiffs.tif': [
+        'version', 'version'],
+    'tests/data/video_MP2T/valid_.ts': ['version']}
 NONE_ELEMENTS_EXTRA = {
-    'tests/data/application_x-internet-archive/valid_1.0_.arc.gz': 'version'}
+    'tests/data/application_x-internet-archive/valid_1.0_.arc.gz':
+    ['version']}
 
 # These are actually valid with another mimetype or version
 # invalid_1.4_wrong_version.pdf -- is valid PDF 1.7
@@ -39,6 +42,10 @@ VALID_MARKED_AS_INVALID = [
 
 
 # These must be ignored due to special parameters or missing scraper
+IGNORE_INVALID = VALID_MARKED_AS_INVALID + [
+    'tests/data/video_x-matroska/invalid__ffv1_missing_data.mkv',
+    'tests/data/video_x-matroska/invalid__ffv1_wrong_duration.mkv']
+
 IGNORE_VALID = ['tests/data/text_xml/valid_1.0_xsd.xml',
                 'tests/data/text_xml/valid_1.0_local_xsd.xml',
                 'tests/data/text_xml/valid_1.0_catalog.xml',
@@ -59,9 +66,9 @@ IGNORE_FOR_METADATA = IGNORE_VALID + [
 # These invalid files are recognized as application/gzip
 DIFFERENT_MIMETYPE_INVALID = {
     'tests/data/application_warc/invalid__missing_data.warc.gz':
-        'application/gzip',
+    'application/gzip',
     'tests/data/application_x-internet-archive/invalid__missing_data.arc.gz':
-        'application/gzip'}
+    'application/gzip'}
 
 
 def test_valid_combined():
@@ -72,13 +79,12 @@ def test_valid_combined():
     - Test that all files are well-formed.
     - Ignore few files because of required parameter or missing scraper.
     """
-    none = {}
     file_dict = get_files(well_formed=True)
     for fullname, value in file_dict.iteritems():
         if fullname in IGNORE_VALID:
             continue
         mimetype = value[0]
-        version = value[1]
+        print fullname
 
         scraper = Scraper(fullname)
         scraper.scrape()
@@ -88,19 +94,17 @@ def test_valid_combined():
 
         assert scraper.well_formed
         assert scraper.mimetype == mimetype
-        # assert scraper.version == version  # fails
+        assert scraper.streams not in [None, {}]
 
-        if scraper.streams is None or scraper.streams is {}:
-            none[fullname] = scraper.streams
-            continue
+        none = []
         for _, stream in scraper.streams.iteritems():
             for key, stream_value in stream.iteritems():
                 if stream_value is None:
-                    if fullname in none:
-                        none[fullname] = none[fullname] + ', ' + key
-                    else:
-                        none[fullname] = key
-    assert none == NONE_ELEMENTS
+                    none.append(key)
+        if fullname in NONE_ELEMENTS:
+            assert none == NONE_ELEMENTS[fullname]
+        else:
+            assert none == []
 
 
 def test_invalid_combined():
@@ -113,13 +117,11 @@ def test_invalid_combined():
     - Skip empty files, since those are detected as inode/x-empty
       and scraper is not found.
     """
-    mime = {}
-    well = {}
     file_dict = get_files(well_formed=False)
-    for fullname, value in file_dict.iteritems():
+    for fullname, _ in file_dict.iteritems():
         if 'empty' in fullname:
             continue
-        if fullname in VALID_MARKED_AS_INVALID:
+        if fullname in IGNORE_INVALID:
             continue
         print fullname
 
@@ -127,20 +129,16 @@ def test_invalid_combined():
         scraper = Scraper(fullname)
         scraper.scrape()
 
-        scraper_found = True
-        if scraper.well_formed is None:
-            for _, info in scraper.info.iteritems():
-                if info['class'] == 'ScraperNotFound':
-                    scraper_found = False
+        skip = False
+        for _, info in scraper.info.iteritems():
+            if scraper.mimetype != mimetype and \
+                    info['class'] == 'ScraperNotFound':
+                skip = True
 
-        if scraper_found:
-            if scraper.well_formed != False:
-                well[fullname] = scraper.well_formed
-            if scraper.mimetype != mimetype:
-                mime[fullname] = scraper.mimetype 
-
-    assert mime == DIFFERENT_MIMETYPE_INVALID
-    assert well == {}
+        if not skip:
+            assert scraper.well_formed is False  # Could be also None (wrong)
+            assert scraper.mimetype == mimetype or \
+                fullname in DIFFERENT_MIMETYPE_INVALID
 
 
 def test_without_wellformed():
@@ -152,12 +150,8 @@ def test_without_wellformed():
       and text.
     - Test a random element existence for image, video, audio and text.
     """
-    well = {}
-    mime = {}
-    none = {}
-    stream_dict = {}
     file_dict = get_files(well_formed=True)
-    for fullname, value in file_dict.iteritems():
+    for fullname, _ in file_dict.iteritems():
         if fullname in IGNORE_FOR_METADATA:
             continue
         print fullname
@@ -166,49 +160,33 @@ def test_without_wellformed():
         scraper = Scraper(fullname)
         scraper.scrape(False)
 
-        if scraper.well_formed is not None:
-            well[fullname] = scraper.well_formed
+        assert scraper.well_formed is None
+        assert scraper.mimetype == mimetype
+        assert scraper.streams not in [None, {}]
+        assert 'stream_type' in scraper.streams[0]
+        assert scraper.streams[0]['stream_type'] is not None
 
-        if scraper.mimetype != mimetype:
-            mime[fullname] = scraper.mimetype
-            continue
-
-        if scraper.streams is None or scraper.streams is {}:
-            none[fullname] = scraper.streams
-            continue
+        none = []
         for _, stream in scraper.streams.iteritems():
-            for key, value in stream.iteritems():
-                if value is None:
-                    if fullname in none:
-                        none[fullname] = none[fullname] + ', ' + key
-                    else:
-                        none[fullname] = key
+            for key, stream_value in stream.iteritems():
+                if stream_value is None:
+                    none.append(key)
+        if fullname in NONE_ELEMENTS:
+            assert none == NONE_ELEMENTS[fullname]
+        else:
+            assert none == []
 
         mimepart = mimetype.split("/")[0]
-        if 'stream_type' not in scraper.streams[0] or \
-                scraper.streams[0]['stream_type'] is None:
-            stream_dict[fullname] = None
+        assert mimepart not in ['image', 'video', 'text', 'audio'] or \
+            mimepart in scraper.streams[0]['stream_type']
 
-        if mimepart in ['image', 'video', 'text', 'audio'] and \
-                mimepart not in scraper.streams[0]['stream_type']:
-            stream_dict[fullname] = scraper.streams[0]
-                    
+        elem_dict = {'image': 'colorspace', 'video': 'color',
+                     'videocontainer': 'codec_name',
+                     'text': 'charset', 'audio': 'num_channels'}
         for _, stream in scraper.streams.iteritems():
-            if 'stream_type' not in stream:
-                stream_dict[fullname] = stream
-                continue
-            elem_dict = {'image': 'colorspace', 'video': 'color',
-                         'videocontainer': 'codec_name',
-                         'text': 'charset', 'audio': 'num_channels'}
-            if stream['stream_type'] in elem_dict and \
-                    elem_dict[stream['stream_type']] not in stream:
-                stream_dict[fullname] = stream
+            assert 'stream_type' in stream
+            if stream['stream_type'] in elem_dict:
+                assert elem_dict[stream['stream_type']] in stream
 
         if 'text/csv' in mimetype:
-            if 'delimiter' not in scraper.streams[0]:
-                stream_dict[fullname] = stream
-
-    assert mime == {}
-    assert stream_dict == {}
-    assert none == NONE_ELEMENTS
-    assert well == {}
+            assert 'delimiter' in scraper.streams[0]
