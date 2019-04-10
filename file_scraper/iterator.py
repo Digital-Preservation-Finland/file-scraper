4b0338d930d95bf5a689a249ffbaf3101cb2fac9
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2019-04-10 14:27:15 +0300

    KDKPAS-1964 Remove the old wand scraper and imports to that and other old scrapers from iterator.

--- file_scraper/iterator.py
+++ file_scraper/iterator.py
@@ -1,38 +1,39 @@
 """Scraper iterator."""
 # flake8: noqa
 # pylint:disable=unused-import
-from file_scraper.base import BaseScraper
-from file_scraper.detectors import FidoDetector, MagicDetector
-from file_scraper.jhove_base import JHove
-from file_scraper.magic_base import BinaryMagic, TextMagic
-from file_scraper.mediainfo_base import Mediainfo
-from file_scraper.pil_base import Pil
-from file_scraper.wand.wand_scraper import WandScraper
-from file_scraper.scrapers.jhove import GifJHove, HtmlJHove, JpegJHove, \
-    PdfJHove, TiffJHove, Utf8JHove, WavJHove
-from file_scraper.scrapers.xmllint import Xmllint
-from file_scraper.scrapers.lxml_encoding import XmlEncoding
-from file_scraper.scrapers.warctools import GzipWarctools, WarcWarctools, \
-    ArcWarctools
-from file_scraper.scrapers.ghostscript import GhostScript
-from file_scraper.scrapers.pngcheck import Pngcheck
-from file_scraper.scrapers.csv_scraper import Csv
-from file_scraper.scrapers.mediainfo import MpegMediainfo, WavMediainfo, \
-    MkvMediainfo, MovMediainfo
-from file_scraper.scrapers.ffmpeg import FFMpegWellformed
-from file_scraper.scrapers.office import Office
-from file_scraper.scrapers.magic import TextFileMagic, XmlFileMagic, \
-    HtmlFileMagic, PdfFileMagic, OfficeFileMagic, PngFileMagic, \
-    JpegFileMagic, Jp2FileMagic, TiffFileMagic, XhtmlFileMagic, \
-    ArcFileMagic
-from file_scraper.scrapers.wand import TiffWand, ImageWand
-from file_scraper.scrapers.pil import ImagePil, JpegPil, TiffPil
-from file_scraper.scrapers.pspp import Pspp
-from file_scraper.scrapers.verapdf import VeraPdf
-from file_scraper.scrapers.dpx import Dpx
-from file_scraper.scrapers.vnu import Vnu
-from file_scraper.scrapers.dummy import ScraperNotFound
 
+# old imports stored temporarily
+#from file_scraper.base import BaseScraper
+#from file_scraper.jhove_base import JHove
+#from file_scraper.magic_base import BinaryMagic, TextMagic
+#from file_scraper.mediainfo_base import Mediainfo
+#from file_scraper.pil_base import Pil
+#from file_scraper.scrapers.jhove import GifJHove, HtmlJHove, JpegJHove, \
+#    PdfJHove, TiffJHove, Utf8JHove, WavJHove
+#from file_scraper.scrapers.xmllint import Xmllint
+#from file_scraper.scrapers.lxml_encoding import XmlEncoding
+#from file_scraper.scrapers.warctools import GzipWarctools, WarcWarctools, \
+#    ArcWarctools
+#from file_scraper.scrapers.ghostscript import GhostScript
+#from file_scraper.scrapers.pngcheck import Pngcheck
+#from file_scraper.scrapers.csv_scraper import Csv
+#from file_scraper.scrapers.mediainfo import MpegMediainfo, WavMediainfo
+#from file_scraper.scrapers.ffmpeg import FFMpegWellformed
+#from file_scraper.scrapers.office import Office
+#from file_scraper.scrapers.magic import TextFileMagic, XmlFileMagic, \
+#    HtmlFileMagic, PdfFileMagic, OfficeFileMagic, PngFileMagic, \
+#    JpegFileMagic, Jp2FileMagic, TiffFileMagic, XhtmlFileMagic, \
+#    ArcFileMagic
+#from file_scraper.scrapers.wand import TiffWand, ImageWand
+#from file_scraper.scrapers.pil import ImagePil, JpegPil, TiffPil
+#from file_scraper.scrapers.pspp import Pspp
+#from file_scraper.scrapers.verapdf import VeraPdf
+#from file_scraper.scrapers.dpx import Dpx
+#from file_scraper.scrapers.vnu import Vnu
+#from file_scraper.scrapers.dummy import ScraperNotFound
+
+from file_scraper.detectors import FidoDetector, MagicDetector
+from file_scraper.wand.wand_scraper import WandScraper
 
 def iter_detectors():
     """
