462022e21d9e68f495e5695930629c51e23cc8be
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2019-05-06 10:16:04 +0300

    KDKPAS-1964 Make iterator tests test that the new Scraper classes are iterated through and the iterator pass the tests.

--- file_scraper/iterator.py
+++ file_scraper/iterator.py
@@ -1,37 +1,5 @@
 """Scraper iterator."""
 # flake8: noqa
-# pylint:disable=unused-import
-
-# old imports stored temporarily
-#from file_scraper.base import BaseScraper
-#from file_scraper.jhove_base import JHove
-#from file_scraper.magic_base import BinaryMagic, TextMagic
-#from file_scraper.mediainfo_base import Mediainfo
-#from file_scraper.pil_base import Pil
-#from file_scraper.scrapers.jhove import GifJHove, HtmlJHove, JpegJHove, \
-#    PdfJHove, TiffJHove, Utf8JHove, WavJHove
-#from file_scraper.scrapers.xmllint import Xmllint
-#from file_scraper.scrapers.lxml_encoding import XmlEncoding
-#from file_scraper.scrapers.warctools import GzipWarctools, WarcWarctools, \
-#    ArcWarctools
-#from file_scraper.scrapers.ghostscript import GhostScript
-#from file_scraper.scrapers.pngcheck import Pngcheck
-#from file_scraper.scrapers.csv_scraper import Csv
-#from file_scraper.scrapers.mediainfo import MpegMediainfo, WavMediainfo
-#from file_scraper.scrapers.ffmpeg import FFMpegWellformed
-#from file_scraper.scrapers.office import Office
-#from file_scraper.scrapers.magic import TextFileMagic, XmlFileMagic, \
-#    HtmlFileMagic, PdfFileMagic, OfficeFileMagic, PngFileMagic, \
-#    JpegFileMagic, Jp2FileMagic, TiffFileMagic, XhtmlFileMagic, \
-#    ArcFileMagic
-#from file_scraper.scrapers.wand import TiffWand, ImageWand
-#from file_scraper.scrapers.pil import ImagePil, JpegPil, TiffPil
-#from file_scraper.scrapers.pspp import Pspp
-#from file_scraper.scrapers.verapdf import VeraPdf
-#from file_scraper.scrapers.dpx import Dpx
-#from file_scraper.scrapers.vnu import Vnu
-#from file_scraper.scrapers.dummy import ScraperNotFound
-
 from file_scraper.detectors import FidoDetector, MagicDetector
 from file_scraper.csv.csv_scraper import CsvScraper
 from file_scraper.dummy.dummy_scraper import ScraperNotFound
@@ -47,7 +15,6 @@ from file_scraper.pil.pil_scraper import PilScraper
 from file_scraper.pngcheck.pngcheck_scraper import PngcheckScraper
 from file_scraper.pspp.pspp_scraper import PsppScraper
 from file_scraper.schematron.schematron_scraper import SchematronScraper
-from file_scraper.textfile.textfile_scraper import TextfileScraper
 from file_scraper.verapdf.verapdf_scraper import VerapdfScraper
 from file_scraper.vnu.vnu_scraper import VnuScraper
 from file_scraper.wand.wand_scraper import WandScraper
@@ -85,7 +52,7 @@ def iter_scrapers(mimetype, version, check_wellformed=True, params=None):
                 JHoveHtmlScraper, JHoveJpegScraper, JHoveTiffScraper,
                 JHovePdfScraper, JHoveWavScraper, CsvScraper, FFMpegScraper,
                 LxmlScraper, MediainfoScraper, OfficeScraper, PilScraper,
-                PngcheckScraper, PsppScraper, SchematronScraper, TextfileScraper,
+                PngcheckScraper, PsppScraper, SchematronScraper,
                 VerapdfScraper, VnuScraper, XmllintScraper, GzipWarctoolsScraper,
                 WarcWarctoolsScraper, ArcWarctoolsScraper, MagicScraper]
 
@@ -96,21 +63,3 @@ def iter_scrapers(mimetype, version, check_wellformed=True, params=None):
 
     if not scraper_found:
         yield ScraperNotFound
-
-    # TODO This old iterator can be reinstated when all scrapers are compatible
-    #      with the new design.
-#    # pylint: disable=no-member
-#    if params is None:
-#        params = {}
-#    found_scraper = False
-#
-#    scraper_superclasses = [BaseScraper, BinaryMagic, TextMagic, JHove,
-#                            Mediainfo, Pil, Wand]
-#    for superclass in scraper_superclasses:
-#        for cls in superclass.__subclasses__():
-#            if cls.is_supported(mimetype, version, check_wellformed, params):
-#                found_scraper = True
-#                yield cls
-#
-#    if not found_scraper:
-#        yield ScraperNotFound
