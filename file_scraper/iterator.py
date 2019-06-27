0937f6dd173906f225436c6dd63aa324bad1d445
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2019-06-27 15:05:02 +0300

    KDKPAS-1964 Sort imports alphabetically and scrapers list semi-alphabetically (all WarctoolsScrapers are consecutive, but otherwise everything is in alphabetical order).

--- file_scraper/iterator.py
+++ file_scraper/iterator.py
@@ -1,16 +1,17 @@
 """Scraper iterator."""
 # flake8: noqa
-from file_scraper.detectors import FidoDetector, MagicDetector
 from file_scraper.csv.csv_scraper import CsvScraper
+from file_scraper.detectors import FidoDetector, MagicDetector
 from file_scraper.dpx.dpx_scraper import DpxScraper
 from file_scraper.dummy.dummy_scraper import ScraperNotFound
 from file_scraper.ffmpeg.ffmpeg_scraper import FFMpegScraper
+from file_scraper.ghostscript.ghostscript_scraper import GhostscriptScraper
 from file_scraper.jhove.jhove_scraper import (JHoveGifScraper, JHoveHtmlScraper,
                                               JHoveJpegScraper, JHoveTiffScraper,
                                               JHovePdfScraper, JHoveWavScraper)
 from file_scraper.lxml.lxml_scraper import LxmlScraper
-from file_scraper.mediainfo.mediainfo_scraper import MediainfoScraper
 from file_scraper.magic_scraper.magic_scraper import MagicScraper
+from file_scraper.mediainfo.mediainfo_scraper import MediainfoScraper
 from file_scraper.office.office_scraper import OfficeScraper
 from file_scraper.pil.pil_scraper import PilScraper
 from file_scraper.pngcheck.pngcheck_scraper import PngcheckScraper
@@ -19,11 +20,10 @@ from file_scraper.schematron.schematron_scraper import SchematronScraper
 from file_scraper.verapdf.verapdf_scraper import VerapdfScraper
 from file_scraper.vnu.vnu_scraper import VnuScraper
 from file_scraper.wand.wand_scraper import WandScraper
-from file_scraper.ghostscript.ghostscript_scraper import GhostscriptScraper
-from file_scraper.xmllint.xmllint_scraper import XmllintScraper
 from file_scraper.warctools.warctools_scraper import (GzipWarctoolsScraper,
                                                       WarcWarctoolsScraper,
                                                       ArcWarctoolsScraper)
+from file_scraper.xmllint.xmllint_scraper import XmllintScraper
 
 def iter_detectors():
     """
@@ -49,13 +49,13 @@ def iter_scrapers(mimetype, version, check_wellformed=True, params=None):
     """
     scraper_found = False
 
-    scrapers = [WandScraper, GhostscriptScraper, JHoveGifScraper,
-                JHoveHtmlScraper, JHoveJpegScraper, JHoveTiffScraper,
-                JHovePdfScraper, JHoveWavScraper, CsvScraper, FFMpegScraper,
-                LxmlScraper, MediainfoScraper, OfficeScraper, PilScraper,
-                PngcheckScraper, PsppScraper, SchematronScraper,
-                VerapdfScraper, VnuScraper, XmllintScraper, GzipWarctoolsScraper,
-                WarcWarctoolsScraper, ArcWarctoolsScraper, MagicScraper, DpxScraper]
+    scrapers = [ArcWarctoolsScraper, GzipWarctoolsScraper, WarcWarctoolsScraper,
+                CsvScraper, DpxScraper, FFMpegScraper, GhostscriptScraper,
+                JHoveGifScraper, JHoveHtmlScraper, JHoveJpegScraper,
+                JHovePdfScraper, JHoveTiffScraper, JHoveWavScraper,
+                LxmlScraper, MagicScraper, MediainfoScraper, OfficeScraper,
+                PilScraper, PngcheckScraper, PsppScraper, SchematronScraper,
+                VerapdfScraper, VnuScraper, WandScraper, XmllintScraper]
 
     for scraper in scrapers:
         if scraper.is_supported(mimetype, version, check_wellformed, params):
