7bd18bb34cbf8bc39692f1500f18e1bd97733077
Mikko Vatanen <mikko.vatanen@csc.fi>
2019-11-24 21:51:19 +0200

    KDKPAS-2098 Fixed string handling for the filter function
    Function _filter_duplicate_elements() requires byte strings because it is using
    the xml parser for logic. Also it returns XML as byte string, which needs to be
    encoded back to unicode string for the messages string.

--- file_scraper/schematron/schematron_scraper.py
+++ file_scraper/schematron/schematron_scraper.py
@@ -10,7 +10,7 @@ from file_scraper.base import BaseScraper
 from file_scraper.shell import Shell
 from file_scraper.config import SCHEMATRON_DIRNAME
 from file_scraper.schematron.schematron_model import SchematronMeta
-from file_scraper.utils import encode_path, hexdigest
+from file_scraper.utils import encode_path, hexdigest, ensure_text
 
 
 class SchematronScraper(BaseScraper):
@@ -97,8 +97,8 @@ class SchematronScraper(BaseScraper):
             self._errors.append(shell.stderr)
 
         if not self._verbose and shell.returncode == 0:
-            self._messages.append(
-                self._filter_duplicate_elements(shell.stdout))
+            self._messages.append(ensure_text(
+                self._filter_duplicate_elements(shell.stdout_raw)))
         else:
             self._messages.append(shell.stdout)
 
