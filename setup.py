9e9fead35e1cd3d37512b4ca6468dc590d677794
Johan Kylander <johan.kylander@csc.fi>
2020-04-17 12:34:52 +0300

    SAPA-381 Simpler way of reading version in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 """Installation script for the `file-scraper` package."""
-import re
+from file_scraper import __version__
 
 from setuptools import setup, find_packages
-
-with open('file_scraper/__init__.py', 'r') as f:
-    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 setup(
     name='file_scraper',
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
-    version=version,
+    version=__version__,
     entry_points={'console_scripts': [
         'scraper=file_scraper.cmdline:main']},
     zip_safe=False,
