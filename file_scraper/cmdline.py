526978663a5b79aed8f464afb44f81d515aeed38
Vili Oja <vili.oja@csc.fi>
2020-05-29 11:39:50 +0300

    KDKPAS-2375 Add mimetype and version options
    Mimetype and version are explicit click options. The user can also
    pass any extra arguments they want, but the parsing of those needs
    more work.

--- file_scraper/cmdline.py
+++ file_scraper/cmdline.py
@@ -4,8 +4,8 @@ Command line interface for file-scraper
 
 from __future__ import print_function
 
-import click
 import json
+import click
 
 from file_scraper.scraper import Scraper
 
@@ -15,7 +15,10 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command("scrape-file", context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True,
+))
 @click.argument("filename", type=click.Path(exists=True))
 @click.option("--no-wellformedness", "check_wellformed",
               default=True, flag_value=False,
@@ -24,13 +27,25 @@ def cli():
 @click.option("--tool-info", default=False, is_flag=True,
               help="Include errors and messages from different 3rd party "
                    "tools that were used")
-def scrape_file(filename, check_wellformed, tool_info):
+@click.option("--mimetype", default=None,
+              help="Specify the mimetype of the file")
+@click.option("--version", default=None,
+              help="Specify version for the filetype")
+@click.pass_context
+def scrape_file(ctx, filename, check_wellformed, tool_info, mimetype, version):
     """
     Identify file type, collect metadata, and optionally check well-formedness.
 
     TODO describe more
     """  # TODO
-    scraper = Scraper(filename, check_wellformed=check_wellformed)
+    # Turn the list of extra arguments into a dict that can be passed to scraper
+    # and append the explicit click options to that
+    params_split = [element for item in ctx.args for element in item.split("=")]
+    params_dict = dict(zip(params_split[::2], params_split[1::2]))
+    params_dict.update(check_wellformed=check_wellformed,
+                       mimetype=mimetype, version=version)
+
+    scraper = Scraper(filename, **params_dict)
     scraper.scrape()
 
     results = {
