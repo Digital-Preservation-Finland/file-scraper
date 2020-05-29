0341b2c2599b04e97c724730a7af0cf0230d2ab2
Vili Oja <vili.oja@csc.fi>
2020-05-29 16:25:23 +0300

    KDKPAS-2375 Properly interpret extra arguments

--- file_scraper/cmdline.py
+++ file_scraper/cmdline.py
@@ -36,11 +36,15 @@ def scrape_file(ctx, filename, check_wellformed, tool_info, mimetype, version):
     """
     Identify file type, collect metadata, and optionally check well-formedness.
 
+    In addition to the given options, the user can provide any extra arguments
+    that are passed onto the scraper. The arguments must be in the form
+    ``key=value``. Only string and boolean values are possible.
     TODO describe more
     """  # TODO
-    # Turn the list of extra arguments into a dict that can be passed to scraper
-    # and append the explicit click options to that
+    # Turn the list of extra arguments into a dict that can be passed to
+    # the scraper, and append the explicit click options to that
     params_split = [element for item in ctx.args for element in item.split("=")]
+    params_split = [_string_to_bool(element) for element in params_split]
     params_dict = dict(zip(params_split[::2], params_split[1::2]))
     params_dict.update(check_wellformed=check_wellformed,
                        mimetype=mimetype, version=version)
@@ -60,6 +64,15 @@ def scrape_file(ctx, filename, check_wellformed, tool_info, mimetype, version):
         results["tool_info"] = scraper.info
 
     click.echo(json.dumps(results, indent=4))
+
+
+def _string_to_bool(element):
+    if element.lower() == "true":
+        element = True
+    elif element.lower() == "false":
+        element = False
+
+    return element
 
 
 if __name__ == "__main__":
