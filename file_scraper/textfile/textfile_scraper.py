3bf2310222a62e7afe1a18fec908eed1acf20cb7
Johan Kylander <johan.kylander@csc.fi>
2021-05-21 16:34:16 +0300

    SAPA-749 Use charset.upper() in decode_chunk function

--- file_scraper/textfile/textfile_scraper.py
+++ file_scraper/textfile/textfile_scraper.py
@@ -294,7 +294,7 @@ class TextEncodingScraper(BaseScraper):
                      "\x18", "\x19", "\x1A", "\x1B", "\x1C", "\x1D", "\x1E",
                      "\x1F", "\x7F"]
 
-        if charset == 'ISO-8859-15':
+        if charset.upper() == 'ISO-8859-15':
             # Add ISO-8859-15 control characters to the list of forbidden
             # characters
             forbidden += ["\x80", "\x81", "\x82", "\x83", "\x84", "\x85",
