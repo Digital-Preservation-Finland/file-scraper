a4bebe3f9fcf6783f3abed77c037d3ad5d20869d
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2021-11-12 14:12:55 +0000

    KDKPAS-2606 Refactor `defaults_test`
    Now using test parametrization instead of a for-loop in the test, and
    directly fetching the values of the dictionary while looping.

--- tests/defaults_test.py
+++ tests/defaults_test.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """Module to test the default constants that were set."""
 
+import pytest
+
 from file_scraper.defaults import PRONOM_DICT, VERSION_DICT
 
 
-def test_constants_syntax():
+@pytest.mark.parametrize(
+    ["constant_dict", "expected_type"],
+    [
+        [PRONOM_DICT, tuple],
+        [VERSION_DICT, dict],
+    ]
+)
+def test_constants_syntax(constant_dict, expected_type):
     """A simple test to ensure that the two complicated constants, PRONON_DICT
     and VERSION_DICT, will have appropriate type for their value.
     """
-    for constant, expected_type in ((PRONOM_DICT, tuple),
-                                    (VERSION_DICT, dict)):
-        for key in constant:
-            assert isinstance(constant[key], expected_type)
+    for value in constant_dict.values():
+        assert isinstance(value, expected_type)
