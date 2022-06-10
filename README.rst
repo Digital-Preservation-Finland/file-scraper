886e26a4da367ff129e99a44e6819d89bf7ed30e
Juha Lehtonen <juha.lehtonen@csc.fi>
2022-06-10 19:05:00 +0300

    KDKPAS-2852 Fix requirements in setup file.

--- README.rst
+++ README.rst
@@ -24,7 +24,7 @@ Run the following to activate the virtual environment::
 
 Install the required software with commands::
 
-    pip install --upgrade pip setuptools
+    pip install --upgrade pip==20.2.4 setuptools
     pip install -r requirements_github.txt
     pip install .
 
--- setup.py
+++ setup.py
@@ -8,7 +8,20 @@ setup(
     include_package_data=True,
     version=__version__,
     install_requires=[
-        "click"
+        "click",
+        "python-mimeparse",
+        "six",
+        "olefile",
+        "file-magic",
+        "pymediainfo",
+        "Pillow==6.0",
+        "wand==0.6.1",
+        "lxml",
+        "pyexiftool==0.1",
+        "git+https://gitlab.ci.csc.fi/dpres/ffmpeg-python.git"
+        "@develop#egg=ffmpeg_python",
+        "git+https://gitlab.ci.csc.fi/dpres/fido.git"
+        "@develop#egg=opf_fido"
     ],
     entry_points={'console_scripts': [
         'scraper=file_scraper.cmdline:cli']},
