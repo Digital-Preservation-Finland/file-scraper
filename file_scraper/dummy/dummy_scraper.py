9363aa73ec8c83ebefb83d8f76182be575f9571b
Vilma <vilma.kangas@csc.fi>
2023-08-22 11:56:23 +0300

    KDKPAS-2800 Add note on formatVersion not being supported with mimetype text/plain

--- file_scraper/dummy/dummy_scraper.py
+++ file_scraper/dummy/dummy_scraper.py
@@ -122,9 +122,13 @@ class MimeMatchScraper(NoWellformednessBaseScraper):
             else:
                 self._errors.append("File format version is not supported.")
         elif self._predefined_version not in [ver, None]:
-            self._errors.append(
-                "Predefined version '{}' and resulted version '{}' "
-                "mismatch.".format(self._predefined_version, ver))
+            if mime == "text/plain":
+                self._errors.append("Element 'formatVersion' is not supported with mimetype '{}'."
+                                    .format(mime))
+            else:
+                self._errors.append(
+                    "Predefined version '{}' and resulted version '{}' "
+                    "mismatch.".format(self._predefined_version, ver))
 
         self._messages.append("MIME type and file format version checked.")
 
