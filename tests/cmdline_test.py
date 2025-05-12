fd46c53a86ea30a80aa6461b34e9ade02ce4b4f7
Henri Johansson <hjohanss@csc.fi>
2025-05-12 13:09:38 +0000

    TPASPKT-679 Add initial test file
    Contains failing tests, need to be cleaned with rebase before merging.

---
+++ tests/cmdline_test.py
@@ -0,0 +1,40 @@
+import pytest
+from click.testing import CliRunner
+from file_scraper.cmdline import cli
+
+data = "tests/data/"
+
+
+def _get_cmdline_options():
+    return ["--skip-wellformed-check", "--tool-info",
+            "--version", "--mimetype"]
+
+
+def test_group_command_lists_commands():
+    runner = CliRunner()
+    result = runner.invoke(cli, [])
+    assert result.exit_code == 0
+    assert "scrape-file" in result.output
+
+
+def test_scraper_without_arguments():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["scrape-file"])
+    assert result.exit_code == 2
+    assert "Error: Missing argument 'FILENAME'" in result.output
+
+
+@pytest.mark.parametrize(
+    "filename, flag, output",
+    [(data + "application_pdf/valid_A-1a", "", ""),
+        (data + "application_pdf/valid_A-1a", "--version=1.5", ""),
+        (data + "application_pdf/valid_A-1a", "--tool-info", "")])
+def test_flags_change_output(filename, flag, output):
+    """"""
+
+    runner = CliRunner()
+    result = runner.invoke(cli, ["scrape-file", filename, flag])
+    if flag:
+        result_noflag = runner.invoke(cli, ["scrape-file", filename])
+        assert result != result_noflag
+    assert result.exit_code == 0
