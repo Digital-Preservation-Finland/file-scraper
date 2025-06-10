274fa90606d978ab7a16baf6f616d429d871d393
Henri Johansson <hjohanss@csc.fi>
2025-06-10 07:43:15 +0000

    TPASPKT-1507 Add tests for paths.py
    Altered test.conf to match the new style of the configuration file.

--- tests/paths_test.py
+++ tests/paths_test.py
@@ -1,50 +1,58 @@
 """Tests for config.py."""
 import pytest
 
-from file_scraper.paths import resolve_path, SOFTWARE_LOCATIONS as S_L
+from file_scraper.paths import resolve_command, resolve_path_from_config
 
 
 @pytest.fixture(scope="function")
 def config_file_fx(monkeypatch):
     """
     Set the environment variable 'FILE_SCRAPER_CONFIG' to a test configuration
-    file."""
+    file.
+    """
     monkeypatch.setenv("FILE_SCRAPER_CONFIG", "tests/config/test.conf")
-    monkeypatch.setitem(S_L["PSPP"],
-                        "config_name", "pspp_path")
 
 
 @pytest.mark.usefixtures("config_file_fx")
-def test_get_value_edited_configfile():
-    """Test 'get_value' function with an edited configuration file. If a new
-    value is set in the configuration file, 'get_value' returns it. Otherwise
-    it returns the default value.
+def test_resolve_command_with_edited_configfile():
     """
-    pspp_value = resolve_path(S_L["PSPP"])
-    assert pspp_value == "/test/path/test/path"
+    Test 'resolve_command' function with an edited configuration file. If the
+    command is not in the configuration file resolve the command from $PATH
+    otherwise raises an error which is tested elsewhere
+    """
+    pspp_exec = resolve_command("pspp-convert")
+    assert pspp_exec == "/test/path/test/pspp-convert"
 
-    schematron_value = resolve_path(S_L["SCHEMATRON_DIR"])
+    # Executable which is not in the configuration file still found from $PATH
+    gs_exec = resolve_command("gs")
+    assert gs_exec == "/usr/bin/gs"
+
+
+@pytest.mark.usefixtures("config_file_fx")
+def test_resolve_path_from_config_with_edited_configfile():
+    """
+    Test 'resolve_path_from_config' will only attempt to find the value from
+    the configuration file otherwise raises an error which is tested elsewhere.
+    """
+    schematron_value = resolve_path_from_config("schematron_dir")
     assert schematron_value == "/usr/share/iso_schematron_xslt1/"
 
 
 @pytest.mark.parametrize("info, path", [
-    (S_L["PSPP"], "/usr/bin/pspp-convert"),
-    (S_L["SCHEMATRON_DIR"], "/usr/share/iso_schematron_xslt1/"),
-    (S_L["VERAPDF"], "/usr/bin/verapdf"),
-    (S_L["VNU"], "/usr/bin/vnu"),
-    (S_L["SOFFICE"], "/opt/libreoffice24.8/program/soffice"),
-    (S_L["GHOSTSCRIPT"], "/opt/ghostscript-10.03/bin/gs")])
-def test_get_value_default_values(info, path):
-    """Test resolve_path function with some values."""
-    value = resolve_path(info)
+    ("schematron_dir", "/usr/share/iso_schematron_xslt1/"),
+    ("magiclib", "/opt/file-5.45/lib64/libmagic.so.1")])
+def test_resolve_path_from_config(info, path):
+    value = resolve_path_from_config(info)
     assert value == path
 
 
 def test_invalid_config_and_command_parameter():
     """
-    Test that values which are not included in the config file or
-    cannot be resolved from path raise a NameError.
+    Test that function resolve_command raises a NameError when values cannot
+    be found from the configuration file or path.
+    Also test resolve_path_from_config which searches only configuration file
     """
     with pytest.raises(NameError):
-        resolve_path({"config_name": "nowaythisconfigactuallyexists",
-                      "command": None})
+        resolve_command("nowaythisconfigactuallyexists")
+    with pytest.raises(NameError):
+        resolve_path_from_config("nowaythisconfiactuallyexists")
