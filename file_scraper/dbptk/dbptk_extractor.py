7b3c2145634124b91525e6a4644a0d5157ce16e5
Joel Weinitschke <joel.weinitschke@csc.fi>
2025-08-11 13:41:59 +0300

    KDKPAS-3618 Fix dbptk_scraper formatting
    - Remove \d from regex; \w already inludes digits.
    - Turn string into comment.

--- file_scraper/dbptk/dbptk_extractor.py
+++ file_scraper/dbptk/dbptk_extractor.py
@@ -3,6 +3,7 @@
 This extractor does not scrape metadata but instead checks well-formedness
 of SIARD files.
 """
+from __future__ import annotations
 
 import os
 import re
@@ -22,12 +23,18 @@ class DbptkExtractor(BaseExtractor):
     _supported_metadata = [DbptkMeta]
     _only_wellformed = True  # Only well-formed check
 
-    def __init__(self, filename: Path, mimetype, version=None, params=None):
+    def __init__(
+        self,
+        filename: Path,
+        mimetype: str,
+        version: str | None = None,
+        params: dict | None = None,
+    ) -> None:
         super().__init__(filename, mimetype, version, params)
         self._path = f"/usr/lib/jvm/jre-1.8.0/bin{os.pathsep} \
         {os.environ['PATH']}"
 
-    def extract(self):
+    def extract(self) -> None:
         """Scrape file using dbptk."""
         # Set $PATH so that Java 8 is favored if it's installed
 
@@ -48,7 +55,7 @@ class DbptkExtractor(BaseExtractor):
         self.streams = list(self.iterate_models())
         self._check_supported(allow_unav_version=True)
 
-    def tools(self):
+    def tools(self) -> dict:
         """
         Overwriting baseclass implementation
         to collect information about software used by the extractor
@@ -58,16 +65,16 @@ class DbptkExtractor(BaseExtractor):
         # command without arguments prints default output,
         # which includes the version
         tool_shell = Shell(["dbptk"], env={"PATH": self._path})
-        """
-        Regex for finding the version of dbptk
 
-        Find string "DBPTK Developer (version"
-        optionally capture group of words, numbers, dashes and dots
-        until a closing parenthesis.
-        """
+        # Regex for finding the version of dbptk
+
+        # Find string "DBPTK Developer (version"
+        # optionally capture group of words, numbers, dashes and dots
+        # until a closing parenthesis.
+
         try:
             version = next(
-                re.finditer(r"DBPTK Developer \(version ([\w\d\-.]*)\)$",
+                re.finditer(r"DBPTK Developer \(version ([\w\-.]*)\)$",
                             tool_shell.stdout, re.MULTILINE)
             ).groups()[0]
             return {"DBPTK Developer": {"version":  version}}
