93393d6bcd633f4740b07e330b8d7f20e0cd8436
Janne Pulkkinen <janne.pulkkinen@csc.fi>
2025-12-04 10:09:22 +0000

    TPASPKT-1615 Test JPG/Exif file w/ invalid field
    Per Exif spec the value in `ExifVersion` field must always be four
    characters long and cannot be null-terminated. Some implementations do
    this anyway according to ExifTool.
    
    Add a corresponding file and test case. The file was created by changing
    the value in `valid_2.2.1_exif_metadata.jpg` from `0221` to `022\0`.

--- tests/extractors/exiftool_test.py
+++ tests/extractors/exiftool_test.py
@@ -11,8 +11,9 @@ from pathlib import Path
 
 import pytest
 
-from file_scraper.exiftool.exiftool_extractor import ExifToolDngExtractor
-from tests.common import (parse_results, partial_message_included)
+from file_scraper.exiftool.exiftool_extractor import (ExifToolDngExtractor,
+                                                      ExifToolExifExtractor)
+from tests.common import parse_results, partial_message_included
 
 
 @pytest.mark.parametrize(
@@ -42,6 +43,47 @@ def test_extractor_dng(filename, result_dict, evaluate_extractor):
 
     extractor = ExifToolDngExtractor(filename=correct.filename,
                                    mimetype="image/x-adobe-dng")
+    extractor.extract()
+
+    if correct.well_formed is not False:
+        evaluate_extractor(extractor, correct)
+    else:
+        assert not extractor.well_formed
+        assert partial_message_included(correct.stdout_part,
+                                        extractor.messages())
+        assert partial_message_included(correct.stderr_part,
+                                        extractor.errors())
+
+
+@pytest.mark.parametrize(
+    ["filename", "result_dict"],
+    [
+        (
+            "valid_2.2.1_exif_metadata.jpg", {
+                "purpose": "Test valid file",
+                "stdout_part": "successfully",
+                "stderr_part": ""
+            }
+        ),
+        (
+            "invalid_2.2_null_terminated_exif_version.jpg", {
+                "purpose": "Test file with null-terminated ExifVersion",
+                "stdout_part": "",
+                "stderr_part": "'022' is not 4 characters long"
+            }
+        )
+    ]
+)
+def test_extractor_exif(filename, result_dict, evaluate_extractor):
+    """
+    Test extractor with valid and invalid JPEG/Exif files
+    """
+    correct = parse_results(filename, "image/jpeg", result_dict, False)
+
+    extractor = ExifToolExifExtractor(
+        filename=correct.filename,
+        mimetype="image/jpeg"
+    )
     extractor.extract()
 
     if correct.well_formed is not False:
